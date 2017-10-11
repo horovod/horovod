4db8a0c977378dc5484902043650b0f0976e793b
Alex Sergeev <alexander.sergeev@live.com>
2017-10-11 12:32:55 -0700

    Support for Cray MPI: custom command to discover MPI compilation flags (#36)
    * Support for Cray MPI: custom command to discover MPI compilation flags
    
    * Dynamically determine if first word of clang++ -I/usr/local/Cellar/open-mpi/2.1.1/include -L/usr/local/opt/libevent/lib -L/usr/local/Cellar/open-mpi/2.1.1/lib -lmpi is an argument or a compiler name

--- setup.py
+++ setup.py
@@ -101,15 +101,23 @@ def get_tf_abi(build_ext, include_dirs, lib_dirs, libs):
 
 
 def get_mpi_flags():
+    show_command = os.environ.get('HOROVOD_MPICXX_SHOW', 'mpicxx -show')
     try:
         mpi_show_output = subprocess.check_output(
-            ['mpicxx', '-show'], universal_newlines=True).strip()
+            shlex.split(show_command), universal_newlines=True).strip()
+        mpi_show_args = shlex.split(mpi_show_output)
+        if not mpi_show_args[0].startswith('-'):
+            # Open MPI and MPICH print compiler name as a first word, skip it
+            mpi_show_args = mpi_show_args[1:]
         # strip off compiler call portion and always escape each arg
         return ' '.join(['"' + arg.replace('"', '"\'"\'"') + '"'
-                         for arg in shlex.split(mpi_show_output)[1:]])
+                         for arg in mpi_show_args])
     except Exception:
         raise DistutilsPlatformError(
-            'mpicxx -show failed, is mpicxx in $PATH?\n\n%s' % traceback.format_exc())
+            '%s failed (see error below), is MPI in $PATH?\n'
+            'Note: If your version of MPI has custom command to show compilation flags, '
+            'please specify it via HOROVOD_MPICXX_SHOW environment variable.\n\n'
+            '%s' % (show_command, traceback.format_exc()))
 
 
 def test_compile(build_ext, name, code, libraries=None, include_dirs=None, library_dirs=None, macros=None):
@@ -307,7 +315,7 @@ class custom_build_ext(build_ext):
 
 
 setup(name='horovod',
-      version='0.9.7',
+      version='0.9.8',
       packages=find_packages(),
       description='Distributed training framework for TensorFlow.',
       author='Uber Technologies, Inc.',
