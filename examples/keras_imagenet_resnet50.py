2294080e5e7574e0057538a2205ee47175c7b6eb
Alex Sergeev <alexander.sergeev@live.com>
2018-01-09 12:34:56 -0800

    Add Keras ImageNet training example and LearningRateScheduleCallback (#144)

---
+++ examples/keras_imagenet_resnet50.py
@@ -0,0 +1,153 @@
+#
+# ResNet-50 model training using Keras and Horovod.
+#
+# This model is an example of a computation-intensive model that achieves good accuracy on an image
+# classification task.  It brings together distributed training concepts such as learning rate
+# schedule adjustments with a warmup, randomized data reading, and checkpointing on the first worker
+# only.
+#
+# Note: This model uses Keras native ImageDataGenerator and not the sophisticated preprocessing
+# pipeline that is typically used to train state-of-the-art ResNet-50 model.  This results in ~0.5%
+# increase in the top-1 validation error compared to the single-crop top-1 validation error from
+# https://github.com/KaimingHe/deep-residual-networks.
+#
+from __future__ import print_function
+
+import keras
+from keras import backend as K
+from keras.preprocessing import image
+import tensorflow as tf
+import horovod.keras as hvd
+import os
+
+# Horovod: initialize Horovod.
+hvd.init()
+
+# Horovod: pin GPU to be used to process local rank (one GPU per process)
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+config.gpu_options.visible_device_list = str(hvd.local_rank())
+K.set_session(tf.Session(config=config))
+
+# Settings from https://arxiv.org/abs/1706.02677.
+batch_size = 32
+learning_rate = 0.0125
+warmup_epochs = 5
+weight_decay = 0.00005
+epochs = 90
+
+# Paths for training and validation.
+train_dir = os.path.expanduser('~/imagenet/train')
+test_dir = os.path.expanduser('~/imagenet/validation')
+
+# Checkpoint format and log directory.
+checkpoint_format = './checkpoint-{epoch}.h5'
+log_dir = './logs'
+
+# If set > 0, will resume training from a given checkpoint.
+resume_from_epoch = 0
+for try_epoch in range(epochs, 0, -1):
+    if os.path.exists(checkpoint_format.format(epoch=try_epoch)):
+        resume_from_epoch = try_epoch
+        break
+
+# Horovod: broadcast resume_from_epoch from rank 0 (which will have
+# checkpoints) to other ranks.
+resume_from_epoch = hvd.broadcast(resume_from_epoch, 0, name='resume_from_epoch')
+
+# Horovod: print logs on the first worker.
+verbose = 1 if hvd.rank() == 0 else 0
+
+# Training data iterator.
+train_gen = image.ImageDataGenerator(
+    width_shift_range=0.33, height_shift_range=0.33, zoom_range=0.5, horizontal_flip=True,
+    preprocessing_function=keras.applications.resnet50.preprocess_input)
+train_iter = train_gen.flow_from_directory(train_dir, batch_size=batch_size,
+                                           target_size=(224, 224))
+
+# Validation data iterator.
+test_gen = image.ImageDataGenerator(
+    zoom_range=(0.875, 0.875), preprocessing_function=keras.applications.resnet50.preprocess_input)
+test_iter = test_gen.flow_from_directory(test_dir, batch_size=batch_size,
+                                         target_size=(224, 224))
+
+# Set up standard ResNet-50 model.
+model = keras.applications.resnet50.ResNet50(weights=None)
+
+# ResNet-50 model that is included with Keras is optimized for inference.
+# Add L2 weight decay & adjust BN settings.
+model_config = model.get_config()
+for layer, layer_config in zip(model.layers, model_config['layers']):
+    if hasattr(layer, 'kernel_regularizer'):
+        regularizer = keras.regularizers.l2(weight_decay)
+        layer_config['config']['kernel_regularizer'] = \
+            {'class_name': regularizer.__class__.__name__,
+             'config': regularizer.get_config()}
+    if type(layer) == keras.layers.BatchNormalization:
+        layer_config['config']['momentum'] = 0.9
+        layer_config['config']['epsilon'] = 1e-5
+model = keras.models.Model.from_config(model_config)
+
+# Horovod: adjust learning rate based on number of GPUs.
+opt = keras.optimizers.SGD(lr=learning_rate * hvd.size(), momentum=0.9)
+
+# Horovod: add Horovod Distributed Optimizer.
+opt = hvd.DistributedOptimizer(opt)
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=opt,
+              metrics=['accuracy', 'top_k_categorical_accuracy'])
+
+callbacks = [
+    # Horovod: broadcast initial variable states from rank 0 to all other processes.
+    # This is necessary to ensure consistent initialization of all workers when
+    # training is started with random weights or restored from a checkpoint.
+    hvd.callbacks.BroadcastGlobalVariablesCallback(0),
+
+    # Horovod: average metrics among workers at the end of every epoch.
+    #
+    # Note: This callback must be in the list before the ReduceLROnPlateau,
+    # TensorBoard, or other metrics-based callbacks.
+    hvd.callbacks.MetricAverageCallback(),
+
+    # Horovod: using `lr = 1.0 * hvd.size()` from the very beginning leads to worse final
+    # accuracy. Scale the learning rate `lr = 1.0` ---> `lr = 1.0 * hvd.size()` during
+    # the first five epochs. See https://arxiv.org/abs/1706.02677 for details.
+    hvd.callbacks.LearningRateWarmupCallback(warmup_epochs=warmup_epochs, verbose=verbose),
+
+    # Horovod: after the warmup reduce learning rate by 10 on the 30th, 60th and 80th epochs.
+    hvd.callbacks.LearningRateScheduleCallback(start_epoch=warmup_epochs, end_epoch=30, multiplier=1.),
+    hvd.callbacks.LearningRateScheduleCallback(start_epoch=30, end_epoch=60, multiplier=1e-1),
+    hvd.callbacks.LearningRateScheduleCallback(start_epoch=60, end_epoch=80, multiplier=1e-2),
+    hvd.callbacks.LearningRateScheduleCallback(start_epoch=80, multiplier=1e-3),
+]
+
+# Horovod: save checkpoints only on the first worker to prevent other workers from corrupting them.
+if hvd.rank() == 0:
+    callbacks.append(keras.callbacks.ModelCheckpoint(checkpoint_format))
+    callbacks.append(keras.callbacks.TensorBoard(log_dir))
+
+# Restore from a previous checkpoint, if initial_epoch is specified.
+# Horovod: restore on the first worker which will broadcast weights to other workers.
+if resume_from_epoch > 0 and hvd.rank() == 0:
+    model.load_weights(checkpoint_format.format(epoch=resume_from_epoch))
+
+# Train the model. The training will randomly sample 1 / N batches of training data and
+# 3 / N batches of validation data on every worker, where N is the number of workers.
+# Over-sampling of validation data helps to increase probability that every validation
+# example will be evaluated.
+model.fit_generator(train_iter,
+                    steps_per_epoch=len(train_iter) // hvd.size(),
+                    callbacks=callbacks,
+                    epochs=epochs,
+                    verbose=verbose,
+                    workers=4,
+                    initial_epoch=resume_from_epoch,
+                    validation_data=test_iter,
+                    validation_steps=3 * len(test_iter) // hvd.size())
+
+# Evaluate the model on the full data set.
+score = hvd.allreduce(model.evaluate_generator(test_iter, len(test_iter), workers=4))
+if verbose:
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
--- horovod/keras/callbacks.py
+++ horovod/keras/callbacks.py
@@ -87,11 +87,125 @@ class MetricAverageCallback(keras.callbacks.Callback):
         self._average_metrics_in_place(logs)
 
 
-class LearningRateWarmupCallback(keras.callbacks.Callback):
+class LearningRateScheduleCallback(keras.callbacks.Callback):
+    """
+    LearningRateScheduleCallback sets learning rate between epochs `start_epoch` and
+    `end_epoch` to be `initial_lr * multiplier`.  `multiplier` can be a constant or
+    a function `f(epoch) = lr'`.
+
+    If `multiplier` is a function and `staircase=True`, learning rate adjustment will
+    happen at the beginning of each epoch and the epoch passed to the `multiplier`
+    function will be an integer.
+
+    If `multiplier` is a function and `staircase=False`, learning rate adjustment will
+    happen at the beginning of each batch and the epoch passed to the `multiplier`
+    function will be a floating number: `epoch' = epoch + batch / steps_per_epoch`.
+    This functionality is useful for smooth learning rate adjustment schedulers, such
+    as `LearningRateWarmupCallback`.
+
+    `initial_lr` is the learning rate of the model optimizer at the start of the training.
+    """
+
+    def __init__(self, multiplier, start_epoch=0, end_epoch=None, staircase=True,
+                 momentum_correction=True, steps_per_epoch=None):
+        """
+        Construct a new LearningRateScheduleCallback.
+
+        Args:
+            multiplier: A constant multiplier or a function `f(epoch) = lr'`
+            start_epoch: The first epoch this adjustment will be applied to. Defaults to 0.
+            end_epoch: The epoch this adjustment will stop applying (exclusive end).
+                       Defaults to None.
+            staircase: Whether to adjust learning rate at the start of epoch (`staircase=True`)
+                       or at the start of every batch (`staircase=False`).
+            momentum_correction: Apply momentum correction to optimizers that have momentum.
+                                 Defaults to True.
+            steps_per_epoch: The callback will attempt to autodetect number of batches per
+                             epoch with Keras >= 2.0.0. Provide this value if you have an older
+                             version of Keras.
+        """
+        super(LearningRateScheduleCallback, self).__init__()
+        self.start_epoch = start_epoch
+        self.end_epoch = end_epoch
+        self.staircase = staircase
+        self.momentum_correction = momentum_correction
+        self.initial_lr = None
+        self.restore_momentum = None
+        self.steps_per_epoch = steps_per_epoch
+        self.current_epoch = None
+
+        if not callable(multiplier):
+            self.staircase = True
+            self.multiplier = lambda epoch: multiplier
+        else:
+            self.multiplier = multiplier
+
+    def _autodetect_steps_per_epoch(self):
+        if self.params.get('steps'):
+            # The number of steps is provided in the parameters.
+            return self.params['steps']
+        elif self.params.get('samples') and self.params.get('batch_size'):
+            # Compute the number of steps per epoch using # of samples and a batch size.
+            return self.params['samples'] // self.params['batch_size']
+        else:
+            raise ValueError('Could not autodetect the number of steps per epoch. '
+                             'Please specify the steps_per_epoch parameter to the '
+                             '%s() or upgrade to the latest version of Keras.'
+                             % self.__class__.__name__)
+
+    def _adjust_learning_rate(self, epoch):
+        old_lr = K.get_value(self.model.optimizer.lr)
+        new_lr = self.initial_lr * self.multiplier(epoch)
+        K.set_value(self.model.optimizer.lr, new_lr)
+
+        if hasattr(self.model.optimizer, 'momentum') and self.momentum_correction:
+            # See the paper cited above for more information about momentum correction.
+            self.restore_momentum = K.get_value(self.model.optimizer.momentum)
+            K.set_value(self.model.optimizer.momentum,
+                        self.restore_momentum * new_lr / old_lr)
+
+    def _restore_momentum_if_needed(self):
+        if self.restore_momentum:
+            K.set_value(self.model.optimizer.momentum, self.restore_momentum)
+            self.restore_momentum = None
+
+    def on_train_begin(self, logs=None):
+        self.initial_lr = K.get_value(self.model.optimizer.lr)
+        if not self.staircase and not self.steps_per_epoch:
+            self.steps_per_epoch = self._autodetect_steps_per_epoch()
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.current_epoch = epoch
+
+    def on_batch_begin(self, batch, logs=None):
+        if (self.current_epoch < self.start_epoch or
+                (self.end_epoch is not None and self.current_epoch >= self.end_epoch)):
+            # Outside of the adjustment scope.
+            return
+
+        if self.staircase and batch == 0:
+            # Do on first batch of every epoch.
+            self._adjust_learning_rate(self.current_epoch)
+        elif not self.staircase:
+            epoch = self.current_epoch + float(batch) / self.steps_per_epoch
+            self._adjust_learning_rate(epoch)
+
+    def on_batch_end(self, batch, logs=None):
+        self._restore_momentum_if_needed()
+
+    def on_epoch_end(self, epoch, logs=None):
+        if logs is not None:
+            # Log current learning rate.
+            logs['lr'] = K.get_value(self.model.optimizer.lr)
+
+
+class LearningRateWarmupCallback(LearningRateScheduleCallback):
     """
     Implements gradual learning rate warmup:
 
         `lr = initial_lr / hvd.size()` ---> `lr = initial_lr`
+
+    `initial_lr` is the learning rate of the model optimizer at the start of the training.
 
     This technique was described in the paper "Accurate, Large Minibatch SGD: Training
     ImageNet in 1 Hour". See https://arxiv.org/pdf/1706.02677.pdf for details.
@@ -115,7 +229,7 @@ class LearningRateWarmupCallback(keras.callbacks.Callback):
     def __init__(self, warmup_epochs=5, momentum_correction=True, steps_per_epoch=None,
                  verbose=0):
         """
-        Construct a new LearningRateWarmupCallback that will gradually warmup learning rate.
+        Construct a new LearningRateWarmupCallback that will gradually warm up the learning rate.
 
         Args:
             warmup_epochs: The number of epochs of the warmup phase. Defaults to 5.
@@ -126,62 +240,20 @@ class LearningRateWarmupCallback(keras.callbacks.Callback):
                              version of Keras.
             verbose: verbosity mode, 0 or 1.
         """
-        super(LearningRateWarmupCallback, self).__init__()
-        self.warmup_epochs = warmup_epochs
-        self.momentum_correction = momentum_correction
-        self.initial_lr = None
-        self.restore_momentum = None
-        self.steps_per_epoch = steps_per_epoch
+        def multiplier(epoch):
+            # Adjust epoch to produce round numbers at the end of each epoch, so that TensorBoard
+            # learning rate graphs look better.
+            epoch += 1. / self.steps_per_epoch
+            return 1. / hvd.size() * (epoch * (hvd.size() - 1) / warmup_epochs + 1)
         self.verbose = verbose
-        self.current_epoch = None
+        super(LearningRateWarmupCallback, self).__init__(
+            multiplier, start_epoch=0, end_epoch=warmup_epochs, staircase=False,
+            momentum_correction=momentum_correction, steps_per_epoch=steps_per_epoch)
 
-    def _autodetect_steps_per_epoch(self):
-        if self.params.get('steps'):
-            # The number of steps is provided in the parameters.
-            return self.params['steps']
-        elif self.params.get('samples') and self.params.get('batch_size'):
-            # Compute the number of steps per epoch using # of samples and a batch size.
-            return self.params['samples'] // self.params['batch_size']
-        else:
-            raise ValueError('Could not autodetect the number of steps per epoch. '
-                             'Please specify the steps_per_epoch parameter to the '
-                             'LearningRateWarmupCallback() or upgrade to the latest '
-                             'version of Keras.')
+    def on_epoch_end(self, epoch, logs=None):
+        super(LearningRateWarmupCallback, self).on_epoch_end(epoch, logs)
 
-    def on_train_begin(self, logs=None):
-        self.initial_lr = K.get_value(self.model.optimizer.lr)
-        if not self.steps_per_epoch:
-            self.steps_per_epoch = self._autodetect_steps_per_epoch()
-
-    def on_epoch_begin(self, epoch, logs=None):
-        self.current_epoch = epoch
-
-    def on_batch_begin(self, batch, logs=None):
-        if self.current_epoch > self.warmup_epochs:
-            # Outside of adjustment scope.
-            return
-
-        if self.current_epoch == self.warmup_epochs and batch > 0:
-            # Outside of adjustment scope, final adjustment is done on first batch.
-            return
-
-        old_lr = K.get_value(self.model.optimizer.lr)
-        epoch = self.current_epoch + float(batch) / self.steps_per_epoch
-        new_lr = self.initial_lr / hvd.size() * \
-            (epoch * (hvd.size() - 1) / self.warmup_epochs + 1)
-        K.set_value(self.model.optimizer.lr, new_lr)
-
-        if self.current_epoch == self.warmup_epochs and self.verbose:
-            print('Epoch %d: finished gradual learning rate warmup to %s.' %
+        if epoch == self.end_epoch - 1 and self.verbose > 0:
+            new_lr = K.get_value(self.model.optimizer.lr)
+            print('\nEpoch %d: finished gradual learning rate warmup to %g.' %
                   (epoch + 1, new_lr))
-
-        if hasattr(self.model.optimizer, 'momentum') and self.momentum_correction:
-            # See the paper cited above for more information about momentum correction.
-            self.restore_momentum = K.get_value(self.model.optimizer.momentum)
-            K.set_value(self.model.optimizer.momentum,
-                        self.restore_momentum * new_lr / old_lr)
-
-    def on_batch_end(self, batch, logs=None):
-        if self.restore_momentum:
-            K.set_value(self.model.optimizer.momentum, self.restore_momentum)
-            self.restore_momentum = None
