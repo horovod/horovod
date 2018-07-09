c02ad0c30e7bdbcb4ce340fa9ec035fe1c7735f1
Travis Addair <taddair@uber.com>
2018-07-09 08:54:23 -0700

    Added broadcast tests

--- test/test_keras.py
+++ test/test_keras.py
@@ -26,6 +26,7 @@ import numpy as np
 import os
 import tempfile
 import unittest
+import tensorflow as tf
 
 import horovod.keras as hvd
 
@@ -152,3 +153,52 @@ class KerasTests(unittest.TestCase):
         for weights, new_weights in zip(opt.get_weights(),
                                         new_opt.get_weights()):
             self.assertListEqual(weights.tolist(), new_weights.tolist())
+
+    def test_load_model_broadcast(self):
+        hvd.init()
+
+        def create_model():
+            opt = keras.optimizers.SGD(lr=0.01 * hvd.size(), momentum=0.9)
+            opt = hvd.DistributedOptimizer(opt)
+
+            model = keras.models.Sequential()
+            model.add(keras.layers.Dense(2, input_shape=(3,)))
+            model.add(keras.layers.RepeatVector(3))
+            model.add(keras.layers.TimeDistributed(keras.layers.Dense(3)))
+            model.compile(loss=keras.losses.MSE,
+                          optimizer=opt,
+                          metrics=[keras.metrics.categorical_accuracy],
+                          sample_weight_mode='temporal')
+
+            return model
+
+        model = create_model()
+
+        x = np.random.random((1, 3))
+        y = np.random.random((1, 3, 3))
+        model.train_on_batch(x, y)
+
+        if hvd.rank() == 0:
+            _, fname = tempfile.mkstemp('.h5')
+            model.save(fname)
+
+        K.clear_session()
+
+        if hvd.rank() == 0:
+            model = hvd.load_model(fname)
+            os.remove(fname)
+        else:
+            model = create_model()
+
+        def generator():
+            while 1:
+                yield (x, y)
+
+        callbacks = [hvd.callbacks.BroadcastGlobalVariablesCallback(0)]
+        model.fit_generator(generator(),
+                            steps_per_epoch=10,
+                            callbacks=callbacks,
+                            epochs=2,
+                            verbose=1,
+                            workers=4,
+                            initial_epoch=1)
