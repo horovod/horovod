fb3c0cc7ee9c9735dc0abd0362554fb8f26548ce
Travis Addair <taddair@uber.com>
2018-07-11 09:42:57 -0700

    Imports

--- test/test_torch.py
+++ test/test_torch.py
@@ -17,9 +17,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
-
 import itertools
+import os
 import tempfile
 import torch
 import torch.nn.functional as F
