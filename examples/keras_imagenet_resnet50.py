ed18e31a277df7c75c6e6fe30ccae901aca3a0b9
Travis Addair <tgaddair@gmail.com>
2018-08-02 14:59:28 -0700

    Added command line args to Keras ImageNet examples (#419)
    * Added command line args to Keras ImageNet example in line with those in the PyTorch examples
    
    * Addressed comments
    
    * Fixed checkpoint formatting

--- examples/keras_imagenet_resnet50.py
+++ examples/keras_imagenet_resnet50.py
@@ -13,12 +13,42 @@
 #
 from __future__ import print_function
 
+import argparse
 import keras
 from keras import backend as K
 from keras.preprocessing import image
 import tensorflow as tf
 import horovod.keras as hvd
 import os
+
+parser = argparse.ArgumentParser(description='Keras ImageNet Example',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--train-dir', default=os.path.expanduser('~/imagenet/train'),
+                    help='path to training data')
+parser.add_argument('--val-dir', default=os.path.expanduser('~/imagenet/validation'),
+                    help='path to validation data')
+parser.add_argument('--log-dir', default='./logs',
+                    help='tensorboard log directory')
+parser.add_argument('--checkpoint-format', default='./checkpoint-{epoch}.h5',
+                    help='checkpoint file format')
+
+# Default settings from https://arxiv.org/abs/1706.02677.
+parser.add_argument('--batch-size', type=int, default=32,
+                    help='input batch size for training')
+parser.add_argument('--val-batch-size', type=int, default=32,
+                    help='input batch size for validation')
+parser.add_argument('--epochs', type=int, default=90,
+                    help='number of epochs to train')
+parser.add_argument('--base-lr', type=float, default=0.0125,
+                    help='learning rate for a single GPU')
+parser.add_argument('--warmup-epochs', type=float, default=5,
+                    help='number of warmup epochs')
+parser.add_argument('--momentum', type=float, default=0.9,
+                    help='SGD momentum')
+parser.add_argument('--wd', type=float, default=0.00005,
+                    help='weight decay')
+
+args = parser.parse_args()
 
 # Horovod: initialize Horovod.
 hvd.init()
@@ -29,25 +59,10 @@ config.gpu_options.allow_growth = True
 config.gpu_options.visible_device_list = str(hvd.local_rank())
 K.set_session(tf.Session(config=config))
 
-# Settings from https://arxiv.org/abs/1706.02677.
-batch_size = 32
-learning_rate = 0.0125
-warmup_epochs = 5
-weight_decay = 0.00005
-epochs = 90
-
-# Paths for training and validation.
-train_dir = os.path.expanduser('~/imagenet/train')
-test_dir = os.path.expanduser('~/imagenet/validation')
-
-# Checkpoint format and log directory.
-checkpoint_format = './checkpoint-{epoch}.h5'
-log_dir = './logs'
-
 # If set > 0, will resume training from a given checkpoint.
 resume_from_epoch = 0
-for try_epoch in range(epochs, 0, -1):
-    if os.path.exists(checkpoint_format.format(epoch=try_epoch)):
+for try_epoch in range(args.epochs, 0, -1):
+    if os.path.exists(args.checkpoint_format.format(epoch=try_epoch)):
         resume_from_epoch = try_epoch
         break
 
@@ -62,13 +77,15 @@ verbose = 1 if hvd.rank() == 0 else 0
 train_gen = image.ImageDataGenerator(
     width_shift_range=0.33, height_shift_range=0.33, zoom_range=0.5, horizontal_flip=True,
     preprocessing_function=keras.applications.resnet50.preprocess_input)
-train_iter = train_gen.flow_from_directory(train_dir, batch_size=batch_size,
+train_iter = train_gen.flow_from_directory(args.train_dir,
+                                           batch_size=args.batch_size,
                                            target_size=(224, 224))
 
 # Validation data iterator.
 test_gen = image.ImageDataGenerator(
     zoom_range=(0.875, 0.875), preprocessing_function=keras.applications.resnet50.preprocess_input)
-test_iter = test_gen.flow_from_directory(test_dir, batch_size=batch_size,
+test_iter = test_gen.flow_from_directory(args.val_dir,
+                                         batch_size=args.val_batch_size,
                                          target_size=(224, 224))
 
 # Set up standard ResNet-50 model.
@@ -78,14 +95,14 @@ model = keras.applications.resnet50.ResNet50(weights=None)
 # Horovod: restore on the first worker which will broadcast both model and optimizer weights
 # to other workers.
 if resume_from_epoch > 0 and hvd.rank() == 0:
-    model = hvd.load_model(checkpoint_format.format(epoch=resume_from_epoch))
+    model = hvd.load_model(args.checkpoint_format.format(epoch=resume_from_epoch))
 else:
     # ResNet-50 model that is included with Keras is optimized for inference.
     # Add L2 weight decay & adjust BN settings.
     model_config = model.get_config()
     for layer, layer_config in zip(model.layers, model_config['layers']):
         if hasattr(layer, 'kernel_regularizer'):
-            regularizer = keras.regularizers.l2(weight_decay)
+            regularizer = keras.regularizers.l2(args.wd)
             layer_config['config']['kernel_regularizer'] = \
                 {'class_name': regularizer.__class__.__name__,
                  'config': regularizer.get_config()}
@@ -96,7 +113,8 @@ else:
     model = keras.models.Model.from_config(model_config)
 
     # Horovod: adjust learning rate based on number of GPUs.
-    opt = keras.optimizers.SGD(lr=learning_rate * hvd.size(), momentum=0.9)
+    opt = keras.optimizers.SGD(lr=args.base_lr * hvd.size(),
+                               momentum=args.momentum)
 
     # Horovod: add Horovod Distributed Optimizer.
     opt = hvd.DistributedOptimizer(opt)
@@ -120,10 +138,10 @@ callbacks = [
     # Horovod: using `lr = 1.0 * hvd.size()` from the very beginning leads to worse final
     # accuracy. Scale the learning rate `lr = 1.0` ---> `lr = 1.0 * hvd.size()` during
     # the first five epochs. See https://arxiv.org/abs/1706.02677 for details.
-    hvd.callbacks.LearningRateWarmupCallback(warmup_epochs=warmup_epochs, verbose=verbose),
+    hvd.callbacks.LearningRateWarmupCallback(warmup_epochs=args.warmup_epochs, verbose=verbose),
 
     # Horovod: after the warmup reduce learning rate by 10 on the 30th, 60th and 80th epochs.
-    hvd.callbacks.LearningRateScheduleCallback(start_epoch=warmup_epochs, end_epoch=30, multiplier=1.),
+    hvd.callbacks.LearningRateScheduleCallback(start_epoch=args.warmup_epochs, end_epoch=30, multiplier=1.),
     hvd.callbacks.LearningRateScheduleCallback(start_epoch=30, end_epoch=60, multiplier=1e-1),
     hvd.callbacks.LearningRateScheduleCallback(start_epoch=60, end_epoch=80, multiplier=1e-2),
     hvd.callbacks.LearningRateScheduleCallback(start_epoch=80, multiplier=1e-3),
@@ -131,8 +149,8 @@ callbacks = [
 
 # Horovod: save checkpoints only on the first worker to prevent other workers from corrupting them.
 if hvd.rank() == 0:
-    callbacks.append(keras.callbacks.ModelCheckpoint(checkpoint_format))
-    callbacks.append(keras.callbacks.TensorBoard(log_dir))
+    callbacks.append(keras.callbacks.ModelCheckpoint(args.checkpoint_format))
+    callbacks.append(keras.callbacks.TensorBoard(args.log_dir))
 
 # Train the model. The training will randomly sample 1 / N batches of training data and
 # 3 / N batches of validation data on every worker, where N is the number of workers.
@@ -141,7 +159,7 @@ if hvd.rank() == 0:
 model.fit_generator(train_iter,
                     steps_per_epoch=len(train_iter) // hvd.size(),
                     callbacks=callbacks,
-                    epochs=epochs,
+                    epochs=args.epochs,
                     verbose=verbose,
                     workers=4,
                     initial_epoch=resume_from_epoch,
