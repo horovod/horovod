72bbede4256856ea113f6ec4fec93a9c3f478ee2
Yuechao Pan <yuechaop@nvidia.com>
2018-08-17 13:24:10 -0700

    added copyright notices for DGC related files

--- horovod/common/dgc.cu
+++ horovod/common/dgc.cu
@@ -1,13 +1,22 @@
+// Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =============================================================================
 // DGC host function implementations
-// by Yuechao Pan
-// for NVIDIA
-
 // past compile with following command:
 // nvcc -std=c++11 -c -o dgc.cu.o horovod_nvidia/horovod/common/dgc.cu.cc      \
    -x cu -Xcompiler -fPIC -dlink --expt-extended-lambda                        \
    -gencode=arch=compute_70,code=\"sm_70,compute_70\"
-
-//#pragma once
 
 #include <string>
 #include <algorithm>
