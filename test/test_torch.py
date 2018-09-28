2f4ec7bdd97f1b92433ed8cd4cafcadf2e43e07a
Travis Addair <taddair@uber.com>
2018-09-28 09:35:00 -0700

    Removed imports

--- test/test_torch.py
+++ test/test_torch.py
@@ -17,7 +17,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from distutils.version import LooseVersion
 import inspect
 import itertools
 import numpy as np
