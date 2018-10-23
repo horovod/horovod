752cf367724c6227139edc131ab88484aeb4af2f
Alex Sergeev <alsrgv@users.noreply.github.com>
2018-10-22 22:43:59 -0700

    Add compilation test & runtime identification of AVX/F16C instinsics

--- horovod/common/operations.cc
+++ horovod/common/operations.cc
@@ -23,10 +23,6 @@
 #include <unordered_map>
 #include <unordered_set>
 
-#if __AVX__
-#include <immintrin.h>
-#endif
-
 #if HAVE_CUDA
 #include <cuda_runtime.h>
 #endif
@@ -1412,40 +1408,6 @@ void CheckForStalledTensors(HorovodGlobalState& state) {
       }
       std::cerr << "]" << std::endl;
     }
-  }
-}
-
-// float16 custom data type summation operation.
-void float16_sum(void* invec, void* inoutvec, int* len,
-                 MPI_Datatype* datatype) {
-  // cast invec and inoutvec to your float16 type
-  auto* in = (unsigned short*)invec;
-  auto* inout = (unsigned short*)inoutvec;
-
-  int i;
-#if __AVX__
-  for (i = 0; i < (*len / 8) * 8; i += 8) {
-    // convert in & inout to m256
-    __m256 in_m256 = _mm256_cvtph_ps(_mm_loadu_si128((__m128i*)(in + i)));
-    __m256 inout_m256 = _mm256_cvtph_ps(_mm_loadu_si128((__m128i*)(inout + i)));
-
-    // add them together to new_inout_m256
-    __m256 new_inout_m256 = _mm256_add_ps(in_m256, inout_m256);
-
-    // convert back and store in inout
-    __m128i new_inout_m128i = _mm256_cvtps_ph(new_inout_m256, 0);
-    _mm_storeu_si128((__m128i*)(inout + i), new_inout_m128i);
-  }
-#else
-  i = 0;
-#endif
-  for (; i < *len; ++i) {
-    float in_float;
-    float inout_float;
-    HalfBits2Float(in + i, &in_float);
-    HalfBits2Float(inout + i, &inout_float);
-    inout_float += in_float;
-    Float2HalfBits(&inout_float, inout + i);
   }
 }
 
