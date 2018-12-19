8d09bdafb1663ecc5e341ce129e347e22cf6de56
fardin <fardin@uber.com>
2018-12-19 11:18:03 -0800

    added an explicit case for mapping OUT_OF_RANGE_ERROR to runtime error in pytorch

--- horovod/torch/adapter.cc
+++ horovod/torch/adapter.cc
@@ -146,7 +146,9 @@ void ThrowIfError(Status status) {
     throw std::runtime_error(status.reason());
   case StatusType::INVALID_ARGUMENT:
     throw std::invalid_argument(status.reason());
-  default: // Includes UNKNOWN_ERROR and OUT_OF_RANGE_ERROR
+  case StatusType::OUT_OF_RANGE_ERROR:
+    throw std::runtime_error(status.reason());
+  default: // Includes UNKNOWN_ERROR
     throw std::runtime_error(status.reason());
   }
 }
