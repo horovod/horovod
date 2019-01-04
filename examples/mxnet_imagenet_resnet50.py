24b3f59bb6a821ee2e9a1c926be1a58bfffc5222
Lin Yuan <apeforest@gmail.com>
2019-01-04 13:28:30 -0800

    remove unnecessary waitall

--- examples/mxnet_imagenet_resnet50.py
+++ examples/mxnet_imagenet_resnet50.py
@@ -304,7 +304,7 @@ def main():
 
     # Fetch and broadcast parameters
     (arg_params, aux_params) = mod.get_params()
-    mx.nd.waitall()
+
     if arg_params is not None:
         hvd.broadcast_parameters(arg_params, root_rank=0)
     if aux_params is not None:
@@ -319,7 +319,7 @@ def main():
             batch_end_callback=mx.callback.Speedometer(batch_size, 20),
             optimizer=opt,
             optimizer_params=optimizer_params)
-    mx.nd.waitall()
+
 
 if __name__ == '__main__':
     main()
