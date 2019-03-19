ae7932ead879e170b6341fb08ba1d07b612720c3
Xu Ning <nx@uber.com>
2019-03-18 22:36:52 -0700

    fix test
    Signed-off-by: Xu Ning <nx@uber.com>

--- test/test_stall.py
+++ test/test_stall.py
@@ -1,6 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from mpi4py import MPI
-import horovod.tensorflow as hvd
-import tensorflow as tf
+import horovod.torch as hvd
+import torch
 import time
 import os
 import signal
@@ -8,24 +12,18 @@ from common import env
 
 def test():
   signal.alarm(45)
-  with env(HOROVOD_STALL_CHECK_TIME_SECONDS="2", 
+  with env(HOROVOD_STALL_CHECK_TIME_SECONDS="2",
     HOROVOD_STALL_SHUTDOWN_TIME_SECONDS="5"):
-    config = tf.ConfigProto()
-    tf.enable_eager_execution(config=config)
     hvd.init()
-
-    with tf.device("/cpu:0"):
-        tf.set_random_seed(1234)
-        tensor = tf.constant([[1.0, 2.0], [3.0, 4.0]])
-        if hvd.rank() != 0:
-          time.sleep(10 * hvd.rank());
-        try:
-          hvd.allreduce(tensor, average=False)
-        except:
-          print "except"
-        finally:
-          print "finally"
-          hvd.shutdown()
+    tensor = torch.IntTensor([[1, 2], [3, 4]])
+    if hvd.rank() != 0:
+      time.sleep(10 * hvd.rank());
+    try:
+      summed = hvd.allreduce(tensor, average=False)
+    except:
+      pass
+    finally:
+      hvd.shutdown()
 
 if __name__ == "__main__":
   test()
