da85af25564916b60223f1eb416f8d0d0aff4f5d
Yu, Li-Yu <afg984@gmail.com>
2019-04-15 04:49:45 +0800

    shebang should come before comments (#1013)
    Signed-off-by: afg <afg984@gmail.com>

--- examples/tensorflow_mnist.py
+++ examples/tensorflow_mnist.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Copyright 2017 Uber Technologies, Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-#!/usr/bin/env python
 
 import os
 import errno
