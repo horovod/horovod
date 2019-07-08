378bb17fc73f808a13c00ecac69e5ece0521e1f1
WeichenXu <weichen.xu@databricks.com>
2019-07-08 22:15:17 +0800

    add tests
    Signed-off-by: WeichenXu <weichen.xu@databricks.com>

---
+++ test/test_run_func.py
@@ -0,0 +1,49 @@
+# Copyright 2018 Uber Technologies, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import subprocess
+import time
+import torch
+import traceback
+import unittest
+import warnings
+
+import horovod.torch as hvd
+from horovod.run import run_func
+
+
+class RunFuncTests(unittest.TestCase):
+    """
+    Tests for horovod.run.run_func().
+    """
+    def __init__(self, *args, **kwargs):
+        super(RunFuncTests, self).__init__(*args, **kwargs)
+        warnings.simplefilter('module')
+
+    def test_happy_run(self):
+
+        def fn():
+            hvd.init()
+            res = hvd.allgather(torch.tensor([hvd.rank()])).tolist()
+            if hvd.rank() == 0:
+                return res, hvd.rank()
+
+        res = run_func(fn, num_proc=3, host='localhost:3')
+        self.assertListEqual(([0, 1, 2], 0), res)
