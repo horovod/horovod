23e8f215a08e7d93709ffb27862197aaca34828a
Alex Sergeev <alexander.sergeev@live.com>
2019-07-09 15:03:02 -0700

    Use the first fragment of hostname for host-hash in Spark (#1201)
    * Use the first fragment of hostname for host-hash in Spark
    
    Signed-off-by: Alex Sergeev <alsrgv@users.noreply.github.com>
    
    * Python3 compat
    
    Signed-off-by: Alex Sergeev <alsrgv@users.noreply.github.com>

--- horovod/run/common/util/host_hash.py
+++ horovod/run/common/util/host_hash.py
@@ -33,4 +33,6 @@ def _namespaces():
 def host_hash():
     hostname = socket.gethostname()
     ns = _namespaces()
-    return '%s-%s' % (hostname, hashlib.md5(ns.encode('ascii')).hexdigest())
+    host_ns = hostname + '-' + ns
+    hash = hashlib.md5(host_ns.encode('ascii')).hexdigest()
+    return '%s-%s' % (hostname.split('.')[0], hash)
