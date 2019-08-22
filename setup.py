0f417a39fc778200319eaa8470765be5edca8aa6
Alex Sergeev <alexander.sergeev@live.com>
2019-08-22 14:56:52 -0700

    Fix compiler version check (#1336)
    Signed-off-by: Alex Sergeev <alsrgv@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -798,7 +798,7 @@ def find_matching_gcc_compiler_path(gxx_compiler_version):
             # gcc, or gcc-7, gcc-4.9, or gcc-4.8.5
             compiler = os.path.join(path_dir, bin_file)
             compiler_version = determine_gcc_version(compiler)
-            if compiler_version == gxx_compiler_version:
+            if compiler_version and compiler_version == gxx_compiler_version:
                 return compiler
 
     print('INFO: Unable to find gcc compiler (version %s).' % gxx_compiler_version)
