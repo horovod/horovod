cbddf761f83aece4cb54a6f48fab3ccf3ba981dc
Tix <tix@microsoft.com>
2019-09-26 16:40:38 -0700

    Removed custom build scripts

--- tix_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd test && pip3 uninstall -y horovod && cd ..
-rm -r ./build
-python3 setup.py clean
-HOROVOD_WITHOUT_PYTORCH=1 HOROVOD_WITHOUT_MXNET=1 HOROVOD_WITH_TENSORFLOW=1 python3 setup.py install
