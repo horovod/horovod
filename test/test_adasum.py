1. fixed average when using nccl ring 2. cleaned up env variables

test/test_adasum.py
@@ -36,10 +36,9 @@ class MPITests(tf.test.TestCase):
     def __init__(self, *args, **kwargs):
         super(MPITests, self).__init__(*args, **kwargs)
         warnings.simplefilter('module')
-        hvd.init()
         self.config = tf.ConfigProto()
         self.config.gpu_options.allow_growth = True
-        self.config.gpu_options.visible_device_list = str(hvd.local_rank())
+        #self.config.gpu_options.visible_device_list = str(hvd.local_rank())
 
     def evaluate(self, tensors):
         sess = ops.get_default_session()
@@ -50,8 +49,11 @@ class MPITests(tf.test.TestCase):
             return sess.run(tensors)
 
 
-    def test_horovod_multiple_allreduce_cpu(self):
+    def test_horovod_adasum_multiple_allreduce_cpu(self):
         """Test on CPU that the Adasum correctly computes 2D tensors."""
+        os.environ['HOROVOD_ADASUM'] = 'CPU_TREE'
+        hvd.init()
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(hvd.local_rank())
         size = hvd.size()
         rank0_tensors = [np.asarray([[1.0, 2.0], [3.0, 4.0]]), np.asarray([[9.0, 10.0], [11.0, 12.0]])]
         rank1_tensors = [np.asarray([[5.0, 6.0], [7.0, 8.0]]), np.asarray([[13.0, 14.0], [15.0, 16.0]])]
@@ -76,8 +78,11 @@ class MPITests(tf.test.TestCase):
                 tmp = [t.astype(np_type) for t in expected]
                 self.assertAllClose(tmp, reduced_tensors)
 
-    def test_horovod_multiple_allreduce_gpu(self):
+    def test_horovod_adasum_multiple_allreduce_gpu_tree(self):
         """Test on GPU that the Adasum correctly computes 2D tensors."""
+        os.environ['HOROVOD_ADASUM'] = 'GPU_TREE'
+        hvd.init()
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(hvd.local_rank())
         size = hvd.size()
         print("Testing with {} ranks.".format(size))
 
@@ -96,6 +101,37 @@ class MPITests(tf.test.TestCase):
                 # and away we go: do reduction
                 reduced_tensors = [
                     self.evaluate(hvd.allreduce(tensor, average=False, allreduce_type=AllreduceType.Adasum))
+                    for tensor in tensors
+                ]
+                # cast expected result to the type of the tensorflow values
+                np_type = dtype.as_numpy_dtype
+                tmp = [t.astype(np_type) for t in expected]
+                self.assertAllClose(tmp, reduced_tensors)
+
+    def test_horovod_adasum_multiple_allreduce_gpu_nccl_local_avg(self):
+        """Test on GPU that the Adasum correctly computes 2D tensors."""
+        os.environ['HOROVOD_ADASUM'] = 'GPU_NCCL_LOCAL_AVG'
+        hvd.init()
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(hvd.local_rank())
+
+        size = hvd.size()
+        print("Testing with {} ranks.".format(size))
+
+        rank0_tensors = [np.asarray([[1.0, 2.0], [3.0, 4.0]]), np.asarray([[9.0, 10.0], [11.0, 12.0]])]
+        rank1_tensors = [np.asarray([[5.0, 6.0], [7.0, 8.0]]), np.asarray([[13.0, 14.0], [15.0, 16.0]])]
+        expected = []
+        for a,b in zip(rank0_tensors, rank1_tensors):
+            answer = adasum_reference_operation(a, b) if size > hvd.local_size() else (a + b) / size
+            expected.append(answer)
+        rank_num = hvd.local_rank()
+        for dtype in [tf.float16, tf.float32, tf.float64]:
+            #with tf.device("/gpu:%d" % rank_num):
+                tensors = map(tf.constant, rank0_tensors if rank_num == 0 else rank1_tensors)
+                # cast to the corresponding dtype
+                tensors = map(lambda tensor: tf.cast(tensor, dtype), tensors)
+                # and away we go: do reduction
+                reduced_tensors = [
+                    self.evaluate(hvd.allreduce(tensor, average=True, allreduce_type=AllreduceType.Adasum))
                     for tensor in tensors
                 ]
                 # cast expected result to the type of the tensorflow values