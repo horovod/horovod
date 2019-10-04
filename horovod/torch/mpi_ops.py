Fix bug with the averaging for NCCL_LOCAL_AVG
If the user had set HOROVOD_ADASUM_GPU=NCCL_LOCAL_AVG, but actually
placed tensors on CPU, then the local averaging would still be applied
in framework code. Now the placement of the tensor to be reduced is
checked.

horovod/torch/mpi_ops.py
@@ -95,7 +95,8 @@ def _allreduce_async(tensor, output, name, op):
         divisor = size()
     elif (op == Adasum and
             'HOROVOD_ADASUM_GPU' in os.environ and
-            os.environ['HOROVOD_ADASUM_GPU'].upper() == 'NCCL_LOCAL_AVG'):
+            os.environ['HOROVOD_ADASUM_GPU'].upper() == 'NCCL_LOCAL_AVG' and
+            torch.cuda.is_available() and tensor.device.type != 'cpu'):
         divisor = local_size()
     else:
         divisor = 1