7540382fd30a1ab2a56fcd2c23655181d90badd8
Travis Addair <taddair@uber.com>
2019-12-12 09:04:48 -0800

    Added support for Spark 3 TaskContext to fetch GPU resources per task
    Signed-off-by: Travis Addair <taddair@uber.com>

--- test/test_spark.py
+++ test/test_spark.py
@@ -19,9 +19,12 @@ from __future__ import print_function
 
 import contextlib
 import os
+import platform
 import pytest
 import re
+import stat
 import subprocess
+import tempfile
 import time
 import unittest
 import warnings
@@ -42,11 +45,63 @@ from horovod.run.common.util import secret
 from horovod.run.mpi_run import _get_mpi_implementation_flags
 from horovod.spark.common import constants, util
 from horovod.spark.common.store import HDFSStore
+from horovod.spark.task import get_available_devices
 from horovod.spark.task.task_service import SparkTaskService, SparkTaskClient
 
 from spark_common import spark_session, create_test_data_from_schema, create_xor_data, local_store
 
 from common import tempdir
+
+
+# Spark will fail to initialize correctly locally on Mac OS without this
+if platform.system() == 'Darwin':
+    os.environ['OBJC_DISABLE_INITIALIZE_FORK_SAFETY'] = 'YES'
+
+
+@contextlib.contextmanager
+def spark(app, cores=2, *args):
+    from pyspark import SparkConf
+    from pyspark.sql import SparkSession
+
+    conf = SparkConf().setAppName(app).setMaster('local[{}]'.format(cores))
+    session = SparkSession \
+        .builder \
+        .config(conf=conf) \
+        .getOrCreate()
+
+    try:
+        yield session
+    finally:
+        session.stop()
+
+
+@contextlib.contextmanager
+def spark_cluster(app, cores=2, *args):
+    from pyspark import SparkConf
+    from pyspark.sql import SparkSession
+
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    temp_file.write(b'echo {\\"name\\": \\"gpu\\", \\"addresses\\": [\\"0\\", \\"1\\"]}')
+    temp_file.close()
+    os.chmod(temp_file.name, stat.S_IRWXU | stat.S_IXGRP | stat.S_IRGRP |
+             stat.S_IROTH | stat.S_IXOTH)
+
+    conf = SparkConf().setMaster('local-cluster[{},1,1024]'.format(cores))
+    conf = conf.set("spark.test.home", os.environ.get('SPARK_HOME'))
+    conf = conf.set("spark.worker.resource.gpu.discoveryScript", temp_file.name)
+    conf = conf.set("spark.worker.resource.gpu.amount", 1)
+    conf = conf.set("spark.task.resource.gpu.amount", "1")
+    conf = conf.set("spark.executor.resource.gpu.amount", "1")
+
+    session = SparkSession \
+        .builder \
+        .config(conf=conf) \
+        .getOrCreate()
+
+    try:
+        yield session
+    finally:
+        session.stop()
 
 
 @contextlib.contextmanager
@@ -749,3 +804,13 @@ class SparkTests(unittest.TestCase):
                     env = sorted([line.strip() for line in f.readlines()])
                     expected = ['HADOOP_TOKEN_FILE_LOCATION=HADOOP_TOKEN_FILE_LOCATION value', 'test=value']
                     self.assertEqual(env, expected)
+
+    def test_get_available_devices(self):
+        def fn():
+            hvd.init()
+            devices = get_available_devices()
+            return devices, hvd.local_rank()
+
+        with spark_cluster('test_get_available_devices'):
+            res = horovod.spark.run(fn, env={'PATH': os.environ.get('PATH')}, verbose=0)
+            self.assertListEqual([(['0'], 0), (['1'], 1)], res)
