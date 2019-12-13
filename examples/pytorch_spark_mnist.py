174295c9454e72f370389943d3f39513eac03407
Travis Addair <taddair@uber.com>
2019-12-12 19:41:44 -0800

    Cast input to Float in MNIST example
    Signed-off-by: Travis Addair <taddair@uber.com>

--- examples/pytorch_spark_mnist.py
+++ examples/pytorch_spark_mnist.py
@@ -81,6 +81,7 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
+        x = x.float()
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = x.view(-1, 320)
