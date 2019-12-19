horovod/horovod

commit 5fa1d7aea2c89fdfa15a688aa413ea49a480ab38

Introducing Adasum algorithm to do allreduction. (#1485)
* Introducing Adasum algorithm to do allreduction.
 1. Adasum operations for both CPU and NCCL build of Horovod
 2. Framework support in Tensorflow and Pytorch to enable Adasum
 3. A new optimizer added for Tensorflow and Pytorch to deliver more accurate estimation when using Adasum

Main contributors:
Olli Saarikivi (olsaarik)
Vadim Eksarevskiy (vaeksare)
Jaliya Ekanayake (jaliyae)
Todd Mytkowicz (klipto)
Saeed Maleki(saeedmaleki)
Sergii Dymchenko(kit1980)

Signed-off-by: Tix <tix@microsoft.com>

setup.py
@@ -1,4 +1,5 @@
 # Copyright 2019 Uber Technologies, Inc. All Rights Reserved.
+# Modifications copyright Microsoft
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -112,7 +113,7 @@ def check_avx_supported():
 
 def get_cpp_flags(build_ext):
     last_err = None
-    default_flags = ['-std=c++11', '-fPIC', '-O2', '-Wall']
+    default_flags = ['-std=c++11', '-fPIC', '-O2', '-Wall', '-mf16c', '-mavx', '-mfma', '-fassociative-math', '-ffast-math', '-ftree-vectorize', '-funsafe-math-optimizations']
     avx_flags = ['-mf16c', '-mavx'] if check_avx_supported() else []
     if sys.platform == 'darwin':
         # Darwin most likely will have Clang, which has libc++.
@@ -121,7 +122,7 @@ def get_cpp_flags(build_ext):
                         default_flags + ['-stdlib=libc++'],
                         default_flags]
     else:
-        flags_to_try = [default_flags + avx_flags,
+        flags_to_try = [default_flags + avx_flags + ['-fopt-info-vec-optimized'],
                         default_flags + ['-stdlib=libc++'] + avx_flags,
                         default_flags,
                         default_flags + ['-stdlib=libc++']]
@@ -689,7 +690,9 @@ def get_common_options(build_ext):
         SOURCES += ['horovod/common/half.cc',
                     'horovod/common/mpi/mpi_context.cc',
                     'horovod/common/mpi/mpi_controller.cc',
-                    'horovod/common/ops/mpi_operations.cc']
+                    'horovod/common/ops/mpi_operations.cc',
+                    'horovod/common/ops/adasum/adasum_mpi.cc',
+                    'horovod/common/ops/adasum_mpi_operations.cc']
         COMPILE_FLAGS += shlex.split(mpi_flags)
         LINK_FLAGS += shlex.split(mpi_flags)
 
@@ -715,6 +718,7 @@ def get_common_options(build_ext):
         SOURCES += ['horovod/common/ops/cuda_operations.cc']
         if have_mpi:
             SOURCES += ['horovod/common/ops/mpi_cuda_operations.cc']
+        INCLUDES += ['horovod/common/ops/cuda']
         LIBRARY_DIRS += cuda_lib_dirs
         LIBRARIES += ['cudart']
 
@@ -722,6 +726,8 @@ def get_common_options(build_ext):
         MACROS += [('HAVE_NCCL', '1')]
         INCLUDES += nccl_include_dirs
         SOURCES += ['horovod/common/ops/nccl_operations.cc']
+        if have_mpi:
+            SOURCES += ['horovod/common/ops/adasum_cuda_operations.cc']
         LIBRARY_DIRS += nccl_lib_dirs
         LIBRARIES += nccl_libs
 

examples/pytorch_imagenet_resnet50.py
@@ -30,6 +30,8 @@ parser.add_argument('--batches-per-allreduce', type=int, default=1,
                     help='number of batches processed locally before '
                          'executing allreduce across workers; it multiplies '
                          'total batch size.')
+parser.add_argument('--use-adasum', action='store_true', default=False,
+                    help='use adasum algorithm to do reduction')
 
 # Default settings from https://arxiv.org/abs/1706.02677.
 parser.add_argument('--batch-size', type=int, default=32,
@@ -125,15 +127,21 @@ val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.val_batch_
 # Set up standard ResNet-50 model.
 model = models.resnet50()
 
+# By default, Adasum doesn't need scaling up learning rate.
+# For sum/average with gradient Accumulation: scale learning rate by batches_per_allreduce
+lr_scaler = args.batches_per_allreduce * hvd.size() if not args.use_adasum else 1
+
 if args.cuda:
     # Move model to GPU.
     model.cuda()
+    # If using GPU Adasum allreduce, scale learning rate by local_size.
+    if args.use_adasum and hvd.nccl_built():
+        lr_scaler = args.batches_per_allreduce * hvd.local_size()
 
 # Horovod: scale learning rate by the number of GPUs.
-# Gradient Accumulation: scale learning rate by batches_per_allreduce
 optimizer = optim.SGD(model.parameters(),
                       lr=(args.base_lr *
-                          args.batches_per_allreduce * hvd.size()),
+                          lr_scaler),
                       momentum=args.momentum, weight_decay=args.wd)
 
 # Horovod: (optional) compression algorithm.
@@ -143,7 +151,8 @@ compression = hvd.Compression.fp16 if args.fp16_allreduce else hvd.Compression.n
 optimizer = hvd.DistributedOptimizer(
     optimizer, named_parameters=model.named_parameters(),
     compression=compression,
-    backward_passes_per_step=args.batches_per_allreduce)
+    backward_passes_per_step=args.batches_per_allreduce,
+    op=hvd.Adasum if args.use_adasum else hvd.Average)
 
 # Restore from a previous checkpoint, if initial_epoch is specified.
 # Horovod: restore on the first worker which will broadcast weights to other workers.

examples/pytorch_mnist.py
@@ -27,6 +27,9 @@ parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
 parser.add_argument('--fp16-allreduce', action='store_true', default=False,
                     help='use fp16 compression during allreduce')
+parser.add_argument('--use-adasum', action='store_true', default=False,
+                    help='use adasum algorithm to do reduction')
+
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -89,12 +92,18 @@ class Net(nn.Module):
 
 model = Net()
 
+# By default, Adasum doesn't need scaling up learning rate.
+lr_scaler = hvd.size() if not args.use_adasum else 1
+
 if args.cuda:
     # Move model to GPU.
     model.cuda()
+    # If using GPU Adasum allreduce, scale learning rate by local_size.
+    if args.use_adasum and hvd.nccl_built():
+        lr_scaler = hvd.local_size()
 
-# Horovod: scale learning rate by the number of GPUs.
-optimizer = optim.SGD(model.parameters(), lr=args.lr * hvd.size(),
+# Horovod: scale learning rate by lr_scaler.
+optimizer = optim.SGD(model.parameters(), lr=args.lr * lr_scaler,
                       momentum=args.momentum)
 
 # Horovod: broadcast parameters & optimizer state.
@@ -107,7 +116,8 @@ compression = hvd.Compression.fp16 if args.fp16_allreduce else hvd.Compression.n
 # Horovod: wrap optimizer with DistributedOptimizer.
 optimizer = hvd.DistributedOptimizer(optimizer,
                                      named_parameters=model.named_parameters(),
-                                     compression=compression)
+                                     compression=compression,
+                                     op=hvd.Adasum if args.use_adasum else hvd.Average)
 
 
 def train(epoch):

examples/pytorch_synthetic_benchmark.py
@@ -31,6 +31,9 @@ parser.add_argument('--num-iters', type=int, default=10,
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
 
+parser.add_argument('--use-adasum', action='store_true', default=False,
+                    help='use adasum algorithm to do reduction')
+
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -45,11 +48,17 @@ cudnn.benchmark = True
 # Set up standard model.
 model = getattr(models, args.model)()
 
+# By default, Adasum doesn't need scaling up learning rate.
+lr_scaler = hvd.size() if not args.use_adasum else 1
+
 if args.cuda:
     # Move model to GPU.
     model.cuda()
+    # If using GPU Adasum allreduce, scale learning rate by local_size.
+    if args.use_adasum and hvd.nccl_built():
+        lr_scaler = hvd.local_size()
 
-optimizer = optim.SGD(model.parameters(), lr=0.01)
+optimizer = optim.SGD(model.parameters(), lr=0.01 * lr_scaler)
 
 # Horovod: (optional) compression algorithm.
 compression = hvd.Compression.fp16 if args.fp16_allreduce else hvd.Compression.none
@@ -57,7 +66,8 @@ compression = hvd.Compression.fp16 if args.fp16_allreduce else hvd.Compression.n
 # Horovod: wrap optimizer with DistributedOptimizer.
 optimizer = hvd.DistributedOptimizer(optimizer,
                                      named_parameters=model.named_parameters(),
-                                     compression=compression)
+                                     compression=compression,
+                                     op=hvd.Adasum if args.use_adasum else hvd.Average)
 
 # Horovod: broadcast parameters & optimizer state.
 hvd.broadcast_parameters(model.state_dict(), root_rank=0)

examples/tensorflow_mnist.py
@@ -18,6 +18,7 @@ import errno
 import tensorflow as tf
 import horovod.tensorflow as hvd
 import numpy as np
+import argparse
 
 from tensorflow import keras
 
@@ -25,6 +26,11 @@ layers = tf.layers
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
+# Training settings
+parser = argparse.ArgumentParser(description='Tensorflow MNIST Example')
+parser.add_argument('--use-adasum', action='store_true', default=False,
+                    help='use adasum algorithm to do reduction')
+args = parser.parse_args()
 
 def conv_model(feature, target, mode):
     """2-layer convolution model."""
@@ -111,11 +117,17 @@ def main(_):
         label = tf.placeholder(tf.float32, [None], name='label')
     predict, loss = conv_model(image, label, tf.estimator.ModeKeys.TRAIN)
 
-    # Horovod: adjust learning rate based on number of GPUs.
-    opt = tf.train.AdamOptimizer(0.001 * hvd.size())
+    lr_scaler = hvd.size()
+    # By default, Adasum doesn't need scaling when increasing batch size. If used with NCCL,
+    # scale lr by local_size
+    if args.use_adasum:
+        lr_scaler = hvd.local_size() if hvd.nccl_built() else 1
+
+    # Horovod: adjust learning rate based on lr_scaler.
+    opt = tf.train.AdamOptimizer(0.001 * lr_scaler)
 
     # Horovod: add Horovod Distributed Optimizer.
-    opt = hvd.DistributedOptimizer(opt)
+    opt = hvd.DistributedOptimizer(opt, op=hvd.Adasum if args.use_adasum else hvd.Average)
 
     global_step = tf.train.get_or_create_global_step()
     train_op = opt.minimize(loss, global_step=global_step)

examples/tensorflow_synthetic_benchmark.py
@@ -31,6 +31,8 @@ parser.add_argument('--eager', action='store_true', default=False,
                     help='enables eager execution')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
+parser.add_argument('--use-adasum', action='store_true', default=False,
+                    help='use adasum algorithm to do reduction')
 
 args = parser.parse_args()
 args.cuda = not args.no_cuda
@@ -53,13 +55,19 @@ if args.eager:
 # Set up standard model.
 model = getattr(applications, args.model)(weights=None)
 
-opt = tf.train.GradientDescentOptimizer(0.01)
+lr_scaler = hvd.size()
+# By default, Adasum doesn't need scaling when increasing batch size. If used with NCCL,
+# scale lr by local_size
+if args.use_adasum:
+    lr_scaler = hvd.local_size() if args.cuda and hvd.nccl_built() else 1
+
+opt = tf.train.GradientDescentOptimizer(0.01 * lr_scaler)
 
 # Horovod: (optional) compression algorithm.
 compression = hvd.Compression.fp16 if args.fp16_allreduce else hvd.Compression.none
 
 # Horovod: wrap optimizer with DistributedOptimizer.
-opt = hvd.DistributedOptimizer(opt, compression=compression)
+opt = hvd.DistributedOptimizer(opt, compression=compression, op=hvd.Adasum if args.use_adasum else hvd.Average)
 
 init = tf.global_variables_initializer()
 bcast_op = hvd.broadcast_global_variables(0)

horovod/common/basics.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2019 Uber Technologies, Inc.
-#
+# Modifications copyright Microsoft
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -25,6 +25,10 @@ class HorovodBasics(object):
     def __init__(self, pkg_path, *args):
         full_path = util.get_extension_full_path(pkg_path, *args)
         self.MPI_LIB_CTYPES = ctypes.CDLL(full_path, mode=ctypes.RTLD_GLOBAL)
+
+        self.Average = self.MPI_LIB_CTYPES.horovod_reduce_op_average()
+        self.Sum = self.MPI_LIB_CTYPES.horovod_reduce_op_sum()
+        self.Adasum = self.MPI_LIB_CTYPES.horovod_reduce_op_adasum()
 
     def init(self, comm=None):
         """A function that initializes Horovod.
@@ -114,6 +118,15 @@ class HorovodBasics(object):
             raise ValueError(
                 'Horovod has not been initialized; use hvd.init().')
         return local_rank
+
+    def is_homogeneous(self):
+        """Returns True if the cluster is homogeneous.
+
+        Returns:
+          A boolean value indicating whether every node in the cluster has same number of ranks.
+        """
+        is_homogeneous = self.MPI_LIB_CTYPES.horovod_is_homogeneous()
+        return bool(is_homogeneous)
 
     def mpi_threads_supported(self):
         """A function that returns a flag indicating whether MPI multi-threading is supported.

commit 914d06d7a55848cdd32ca0a5a15d16476d404fbc

Fix non-x86 build failures caused by #1485
Add extra checks for x86 compiler flags and x86 AVX headers/functions

Signed-off-by: Nicolas V Castet <nvcastet@us.ibm.com>

horovod/common/ops/adasum/adasum.h
@@ -17,9 +17,12 @@
 #define HOROVOD_ADASUM_H
 
 #include <cstring>
-#include <emmintrin.h>
 #include <float.h>
+
+#if __AVX__ && __F16C__
+#include <emmintrin.h>
 #include <immintrin.h>
+#endif
 
 #include "../../common.h"
 #include "../../global_state.h"
@@ -101,10 +104,13 @@ protected:
                                               int count, double& dotProduct,
                                               double& anormsq, double& bnormsq,
                                               int layerid) {
+#if __AVX__ && __F16C__
     if (horovod_datatype == DataType::HOROVOD_FLOAT16) {
       ComputeDotAndNormSqrdsfp16((uint16_t*)a, (uint16_t*)b, count, dotProduct,
                                  anormsq, bnormsq, layerid);
-    } else if (horovod_datatype == DataType::HOROVOD_FLOAT32) {
+    } else
+#endif
+    if (horovod_datatype == DataType::HOROVOD_FLOAT32) {
       ComputeDotAndNormSqrds((float*)a, (float*)b, count, dotProduct, anormsq,
                              bnormsq, layerid);
     } else if (horovod_datatype == DataType::HOROVOD_FLOAT64) {
@@ -119,9 +125,12 @@ protected:
                                  double acoeff, void* __restrict__ a,
                                  double bcoeff, void* __restrict__ b,
                                  int layerid) {
+#if __AVX__ && __F16C__
     if (horovod_datatype == DataType::HOROVOD_FLOAT16) {
       ScaledAddfp16(count, acoeff, (uint16_t*)a, bcoeff, (uint16_t*)b, layerid);
-    } else if (horovod_datatype == DataType::HOROVOD_FLOAT32) {
+    } else
+#endif
+    if (horovod_datatype == DataType::HOROVOD_FLOAT32) {
       ScaledAdd(count, acoeff, (float*)a, bcoeff, (float*)b, layerid);
     } else if (horovod_datatype == DataType::HOROVOD_FLOAT64) {
       ScaledAdd(count, acoeff, (double*)a, bcoeff, (double*)b, layerid);
@@ -415,6 +424,8 @@ private:
     }
   }
 
+
+#if __AVX__ && __F16C__
   inline void ComputeDotAndNormSqrdsfp16(const uint16_t* __restrict__ a,
                                          const uint16_t* __restrict__ b,
                                          int len, double& dotProduct,
@@ -534,6 +545,7 @@ private:
     if (7 < len)
       a[7] = (short)_mm_extract_epi16(r, 7);
   }
+#endif
 };
 
 } // namespace common

setup.py
@@ -100,31 +100,33 @@ def check_mx_version():
             'Your MXNet version is outdated.  Horovod requires mxnet>1.3.0')
 
 
-def check_avx_supported():
+def get_supported_instruction_set_flags(flags_to_check):
+    supported = []
     try:
         flags_output = subprocess.check_output(
             'gcc -march=native -E -v - </dev/null 2>&1 | grep cc1',
             shell=True, universal_newlines=True).strip()
         flags = shlex.split(flags_output)
-        return '+f16c' in flags and '+avx' in flags
+        supported = [x for x in flags_to_check if x.replace('-m', '+') in flags]
     except subprocess.CalledProcessError:
-        # Fallback to non-AVX if were not able to get flag information.
-        return False
+        # Fallback to no advanced instruction set flags if were not able to get flag information.
+        pass
+    return supported
 
 
 def get_cpp_flags(build_ext):
     last_err = None
-    default_flags = ['-std=c++11', '-fPIC', '-O2', '-Wall', '-mf16c', '-mavx', '-mfma', '-fassociative-math', '-ffast-math', '-ftree-vectorize', '-funsafe-math-optimizations']
-    avx_flags = ['-mf16c', '-mavx'] if check_avx_supported() else []
+    default_flags = ['-std=c++11', '-fPIC', '-O2', '-Wall', '-fassociative-math', '-ffast-math', '-ftree-vectorize', '-funsafe-math-optimizations']
+    avx_fma_flags = get_supported_instruction_set_flags(['-mf16c', '-mavx', '-mfma'])
     if sys.platform == 'darwin':
         # Darwin most likely will have Clang, which has libc++.
-        flags_to_try = [default_flags + ['-stdlib=libc++'] + avx_flags,
-                        default_flags + avx_flags,
+        flags_to_try = [default_flags + ['-stdlib=libc++'] + avx_fma_flags,
+                        default_flags + avx_fma_flags,
                         default_flags + ['-stdlib=libc++'],
                         default_flags]
     else:
-        flags_to_try = [default_flags + avx_flags + ['-fopt-info-vec-optimized'],
-                        default_flags + ['-stdlib=libc++'] + avx_flags,
+        flags_to_try = [default_flags + avx_fma_flags,
+                        default_flags + ['-stdlib=libc++'] + avx_fma_flags,
                         default_flags,
                         default_flags + ['-stdlib=libc++']]
     for cpp_flags in flags_to_try: