2078ca9056e33550a669beb0107297deca7b8454
fardin abdi <fardin@uber.com>
2020-01-21 10:33:01 -0800

    set pickle protocol to 0 for horovodrun cache
    Signed-off-by: fardin abdi <fardin@uber.com>

--- horovod/run/util/cache.py
+++ horovod/run/util/cache.py
@@ -27,7 +27,7 @@ class Cache(object):
 
     def __init__(self, cache_folder, cache_staleness_threshold_in_minutes,
                  parameters_hash):
-
+        self._pickle_protocol = 0
         self._cache_file = os.path.join(cache_folder, 'cache.bin')
         try:
             # If folder exists, does not do anything.
@@ -37,18 +37,9 @@ class Cache(object):
                 raise
 
         if not os.path.isfile(self._cache_file):
-            with open(self._cache_file, 'wb') as cf:
-                cloudpickle.dump({'parameters_hash': parameters_hash}, cf)
+            self._dump({'parameters_hash': parameters_hash})
 
-        with open(self._cache_file, 'rb') as cf:
-            try:
-                content = cloudpickle.load(cf)
-            except Exception as e:
-                print(
-                    'There is an error with reading cache file. You '
-                    'can delete the corrupt file: {cache_file}.'.format(
-                        cache_file=self._cache_file))
-                raise
+        content = self._load()
 
         if content.get('parameters_hash', None) == parameters_hash:
             # If previous cache was for the same set of parameters, use it.
@@ -75,10 +66,25 @@ class Cache(object):
         self._lock.acquire()
         self._content[key] = (datetime.datetime.now(), val)
         try:
-            with open(self._cache_file, 'wb') as cf:
-                cloudpickle.dump(self._content, cf)
+            self._dump(self._cache_file)
         finally:
             self._lock.release()
+
+    def _dump(self, content):
+        with open(self._cache_file, 'wb') as cf:
+            cloudpickle.dump(content, cf, protocol=self._pickle_protocol)
+
+    def _load(self):
+        with open(self._cache_file, 'rb') as cf:
+            try:
+                content = cloudpickle.load(cf)
+            except Exception as e:
+                print(
+                    'There is an error with reading cache file. You '
+                    'can delete the corrupt file: {cache_file}.'.format(
+                        cache_file=self._cache_file))
+                raise
+        return content
 
 
 def use_cache():
