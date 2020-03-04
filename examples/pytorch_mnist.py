1350555e768b1ae96fdf95fb453b67bcc36e13a8
Nicolas V Castet <nvcastet@us.ibm.com>
2020-03-04 11:58:20 -0600

    Support python 2
    Signed-off-by: Nicolas V Castet <nvcastet@us.ibm.com>

--- examples/pytorch_mnist.py
+++ examples/pytorch_mnist.py
@@ -9,10 +9,18 @@ import horovod.torch as hvd
 
 # Temporary patch this script until the MNIST dataset download issue get resolved
 # https://github.com/pytorch/vision/issues/1938
-from six.moves import urllib
-opener = urllib.request.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-urllib.request.install_opener(opener)
+import urllib
+try:
+    # For python 2
+    class AppURLopener(urllib.FancyURLopener):
+        version = "Mozilla/5.0"
+
+    urllib._urlopener = AppURLopener()
+except AttributeError:
+    # For python 3
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    urllib.request.install_opener(opener)
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
