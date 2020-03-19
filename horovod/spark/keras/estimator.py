dc58b6da91d24b0efa47cadc24ee9a5d358eb645
fardin abdi <fardin@uber.com>
2020-03-19 14:31:40 -0700

    fixed comments
    Signed-off-by: fardin abdi <fardin@uber.com>

--- horovod/spark/keras/estimator.py
+++ horovod/spark/keras/estimator.py
@@ -147,15 +147,15 @@ class KerasEstimator(HorovodEstimator, KerasEstimatorParamsReadable,
                            for more details. Note that this fucntion constructs
                            another function which should perform the
                            transformation.
-         train_reader_num_workers: This parameter specifies the number of parallel processes that
-                                   read the training data from data store and apply data
-                                   transformations to it. Increasing this number
-                                   will generally increase the reading rate but will also
-                                   increase the memory footprint. More processes are
-                                   particularly useful if the bandwidth to the data store is not
-                                   high enough, or users need to apply transformation such as
-                                   decompression or data augmentation on raw data.
-         val_reader_num_workers: Similar to the train_reader_num_workers.
+        train_reader_num_workers: This parameter specifies the number of parallel processes that
+                               read the training data from data store and apply data
+                               transformations to it. Increasing this number
+                               will generally increase the reading rate but will also
+                               increase the memory footprint. More processes are
+                               particularly useful if the bandwidth to the data store is not
+                               high enough, or users need to apply transformation such as
+                               decompression or data augmentation on raw data.
+        val_reader_num_workers: Similar to the train_reader_num_workers.
     """
 
     custom_objects = Param(Params._dummy(), 'custom_objects', 'custom objects')
