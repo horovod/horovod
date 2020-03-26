63b23ae6fda5968ef909cc879da7aa597af344f6
Enrico Minack <github@enrico.minack.dev>
2020-03-26 21:09:39 +0100

    Exclude any GPU Spark Estimators pytests
    Signed-off-by: Enrico Minack <github@enrico.minack.dev>

--- .buildkite/gen-pipeline.sh
+++ .buildkite/gen-pipeline.sh
@@ -237,7 +237,7 @@ run_spark_integration() {
         ":spark: Spark Keras Rossmann Estimator (${test})" \
         "bash -c \"OMP_NUM_THREADS=1 python /horovod/examples/keras_spark_rossmann_estimator.py --num-proc 2 --work-dir /work --data-dir file:///data --epochs 3 --sample-rate 0.01\""
 
-      if [[ ${test} != *-gloo* ]] || [[ ${queue} != *gpu* ]]; then
+      if [[ ${queue} != *gpu* ]]; then
         run_test "${test}" "${queue}" \
           ":spark: PyTests Spark Estimators (${test})" \
           "bash -c \"cd /horovod/test && pytest --forked -v --capture=no test_spark_keras.py test_spark_torch.py\""
