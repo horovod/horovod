ad0c4db863384e8fbb8233440fc7b8f49f714ed1
Enrico Minack <github@enrico.minack.dev>
2020-04-07 22:20:56 +0200

    Elastic gloo_run for Spark
    Signed-off-by: Enrico Minack <github@enrico.minack.dev>

--- horovod/spark/runner.py
+++ horovod/spark/runner.py
@@ -22,12 +22,13 @@ import pyspark
 
 from horovod.run.util.threads import in_thread
 from horovod.spark.task import task_service
-from horovod.spark.gloo_run import gloo_run
+from horovod.spark.gloo_run import gloo_run, gloo_run_elastic
 from horovod.spark.mpi_run import mpi_run
 from horovod.run.runner import is_gloo_used, run_controller
 from horovod.run.common.util import timeout, host_hash, secret
 from horovod.run.common.util import settings as hvd_settings
-from horovod.spark.driver import driver_service, job_id
+from horovod.run.elastic import settings as hvd_elastic_settings
+from horovod.spark.driver import driver_service, host_discovery, job_id
 
 
 MINIMUM_COMMAND_LIFETIME_S = 3
@@ -100,7 +101,7 @@ def _make_spark_thread(spark_context, spark_job_group, driver, result_queue,
             spark_context.setJobGroup(spark_job_group,
                                       "Horovod Spark Run",
                                       interruptOnCancel=True)
-            procs = spark_context.range(0, numSlices=settings.num_proc)
+            procs = spark_context.range(0, numSlices=settings.max_np if settings.elastic else settings.num_proc)
             # We assume that folks caring about security will enable Spark RPC encryption,
             # thus ensuring that key that is passed here remains secret.
             result = procs.mapPartitionsWithIndex(_make_mapper(driver.addresses(), settings, use_gloo)).collect()
@@ -126,6 +127,31 @@ def _launch_job(use_mpi, use_gloo, settings, driver, env, stdout=None, stderr=No
                    use_mpi, lambda: mpi_run(settings, nics, driver, env, stdout, stderr),
                    False, lambda: None,
                    settings.verbose)
+
+
+def _notify_and_register_task_addresses(driver, settings):
+    # wait for num_proc tasks to register
+    driver.wait_for_initial_registration(settings.start_timeout)
+    if settings.verbose >= 2:
+        print('Initial Spark task registration is complete.')
+
+    def notify_and_register(index):
+        task_client = task_service.SparkTaskClient(index,
+                                                   driver.task_addresses_for_driver(index),
+                                                   settings.key, settings.verbose)
+        task_client.notify_initial_registration_complete()
+        next_task_index = (index + 1) % settings.num_proc
+        next_task_addresses = driver.all_task_addresses(next_task_index)
+        task_to_task_addresses = task_client.get_task_addresses_for_task(next_task_index, next_task_addresses)
+        driver.register_task_to_task_addresses(next_task_index, task_to_task_addresses)
+
+    for index in range(settings.num_proc):
+        in_thread(notify_and_register, (index,))
+
+    driver.wait_for_task_to_task_address_updates(settings.start_timeout)
+
+    if settings.verbose >= 2:
+        print('Spark task-to-task address registration is complete.')
 
 
 def run(fn, args=(), kwargs={}, num_proc=None, start_timeout=None,
@@ -237,26 +263,123 @@ def run(fn, args=(), kwargs={}, num_proc=None, start_timeout=None,
     return [results[index] for index in ranks_to_indices]
 
 
-def _notify_and_register_task_addresses(driver, settings):
-    # wait for num_proc tasks to register
-    driver.wait_for_initial_registration(settings.start_timeout)
-    if settings.verbose >= 2:
-        print('Initial Spark task registration is complete.')
+def run_elastic(fn, args=(), kwargs={}, num_proc=None, min_np=None, max_np=None,
+                start_timeout=None, elastic_timeout=None,
+                env=None, stdout=None, stderr=None, verbose=1, nics=None):
+    """
+    Runs Elastic Horovod in Spark.  Runs `num_proc` processes executing `fn` using the same amount of Spark tasks.
 
-    def notify_and_register(index):
-        task_client = task_service.SparkTaskClient(index,
-                                                   driver.task_addresses_for_driver(index),
-                                                   settings.key, settings.verbose)
-        task_client.notify_initial_registration_complete()
-        next_task_index = (index + 1) % settings.num_proc
-        next_task_addresses = driver.all_task_addresses(next_task_index)
-        task_to_task_addresses = task_client.get_task_addresses_for_task(next_task_index, next_task_addresses)
-        driver.register_task_to_task_addresses(next_task_index, task_to_task_addresses)
+    Args:
+        fn: Function to run.
+        args: Arguments to pass to `fn`.
+        kwargs: Keyword arguments to pass to `fn`.
+        num_proc: Number of Horovod processes.  Defaults to `spark.default.parallelism`.
+        start_timeout: Timeout for Spark tasks to spawn, register and start running the code, in seconds.
+                       If not set, falls back to `HOROVOD_SPARK_START_TIMEOUT` environment variable value.
+                       If it is not set as well, defaults to 600 seconds.
+        env: Environment dictionary to use in Horovod run.  Defaults to `os.environ`.
+        stdout: Horovod stdout is redirected to this stream. Defaults to sys.stdout.
+        stderr: Horovod stderr is redirected to this stream. Defaults to sys.stderr.
+        verbose: Debug output verbosity (0-2). Defaults to 1.
+        nics: List of NICs for tcp network communication.
 
-    for index in range(settings.num_proc):
-        in_thread(notify_and_register, (index,))
+    Returns:
+        List of results returned by running `fn` on each rank.
+    """
 
-    driver.wait_for_task_to_task_address_updates(settings.start_timeout)
+    if start_timeout is None:
+        # Lookup default timeout from the environment variable.
+        start_timeout = int(os.getenv('HOROVOD_SPARK_START_TIMEOUT', '600'))
 
-    if settings.verbose >= 2:
-        print('Spark task-to-task address registration is complete.')
+    # nics needs to be a set
+    if nics and not isinstance(nics, set):
+        nics = set(nics)
+
+    spark_context = pyspark.SparkContext._active_spark_context
+    if spark_context is None:
+        raise Exception('Could not find an active SparkContext, are you '
+                        'running in a PySpark session?')
+
+    if num_proc is None:
+        # try spark.dynamicAllocation.initialExecutors
+        num_proc = spark_context.defaultParallelism
+        if verbose >= 1:
+            print('Running %d processes (inferred from spark.default.parallelism)...' % num_proc)
+    else:
+        if verbose >= 1:
+            print('Running %d processes...' % num_proc)
+
+    if min_np is None:
+        # try spark.dynamicAllocation.minExecutors
+        min_np = num_proc
+    if max_np is None:
+        # try spark.dynamicAllocation.maxExecutors
+        max_np = num_proc
+
+    # start Spark driver service and launch settings.num_proc Spark tasks
+    key = secret.make_secret_key()
+    spark_job_group = 'horovod.spark.run.%d' % job_id.next_job_id()
+    driver = driver_service.SparkDriverService(max_np or num_proc,
+                                               fn, args, kwargs,
+                                               key, nics)
+
+    discovery = host_discovery.SparkDriverHostDiscovery(driver)
+
+    tmout = timeout.Timeout(start_timeout,
+                            message='Timed out waiting for {activity}. Please check that you have '
+                                    'enough resources to run all Horovod processes. Each Horovod '
+                                    'process runs in a Spark task. You may need to increase the '
+                                    'start_timeout parameter to a larger value if your Spark resources '
+                                    'are allocated on-demand.')
+    settings = hvd_elastic_settings.ElasticSettings(num_proc=num_proc,
+                                                    min_np=min_np,
+                                                    max_np=max_np,
+                                                    discovery=discovery,
+                                                    verbose=verbose,
+                                                    key=key,
+                                                    start_timeout=tmout,
+                                                    elastic_timeout=elastic_timeout,
+                                                    nics=nics,
+                                                    run_func_mode=True)
+
+    result_queue = queue.Queue(1)
+
+    # launch settings.num_proc / settings.max_np Spark tasks
+    spark_thread = _make_spark_thread(spark_context, spark_job_group, driver,
+                                      result_queue, settings, True)
+    try:
+        # TODO: why are we doing this in static and is it needed for elastic?
+        ## Determine the index grouping based on host hashes.
+        ## Barrel shift until index 0 is in the first host.
+        #host_hashes = list(driver.task_host_hash_indices().keys())
+        #host_hashes.sort()
+        #while 0 not in driver.task_host_hash_indices()[host_hashes[0]]:
+        #    host_hashes = host_hashes[1:] + host_hashes[:1]
+
+        # TODO: host assignment should set this
+        ## Determine the ranks to indicies
+        #driver.set_ranks_to_indices(ranks_to_indices)
+
+        # Run the job
+        if not gloo_built(verbose=(settings.verbose >= 2)):
+            raise ValueError('Gloo support is required to use elastic traiing, but has not been built.  Ensure CMake is '
+                             'installed and reinstall Horovod with HOROVOD_WITH_GLOO=1 to debug the build error.')
+
+        gloo_run_elastic(settings, env, args.command)
+    except:
+        # Terminate Spark job.
+        spark_context.cancelJobGroup(spark_job_group)
+
+        # Re-raise exception.
+        raise
+    finally:
+        spark_thread.join()
+        driver.shutdown()
+
+    # Make sure Spark Job did not fail.
+    driver.check_for_spark_job_failure()
+
+    # If there's no exception, execution results are in this queue.
+    results = result_queue.get_nowait()
+    # TODO: static run reorders results by ranks
+    return results
