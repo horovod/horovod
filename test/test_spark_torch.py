1045e461a962dce20b3717ba48b2c33cde796b95
Enrico Minack <github@enrico.minack.dev>
2020-04-18 11:16:45 +0200

    Do not run test with Spark over Gloo on Python2
    Disables all integration tests and some unit tests
    for Spark over Gloo on Python2
    
    Signed-off-by: Enrico Minack <github@enrico.minack.dev>

--- test/test_spark_torch.py
+++ test/test_spark_torch.py
@@ -15,6 +15,7 @@
 
 from __future__ import absolute_import
 
+import sys
 import unittest
 import warnings
 
@@ -29,6 +30,7 @@ import torch.nn as nn
 from torch.nn import functional as F
 import torch.optim as optim
 
+from horovod.run.runner import is_gloo_used
 import horovod.spark.torch as hvd
 from horovod.spark.common import constants, util
 from horovod.spark.torch import remote
@@ -62,6 +64,9 @@ class SparkTorchTests(unittest.TestCase):
         warnings.simplefilter('module')
 
     def test_fit_model(self):
+        if sys.version_info < (3, 0, 0) and is_gloo_used():
+            self.skipTest('Horovod on Spark over Gloo only supported on Python3')
+
         model = create_xor_model()
         optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
         loss = F.binary_cross_entropy
