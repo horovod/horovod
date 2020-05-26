50f81f27b634bc5d864285ba68cd25839cd56063
Enrico Minack <github@enrico.minack.dev>
2020-05-26 10:18:06 +0200

    Revert any debugging changes (reducing test footprint)
    Signed-off-by: Enrico Minack <github@enrico.minack.dev>

--- .buildkite/gen-pipeline.sh
+++ .buildkite/gen-pipeline.sh
@@ -18,12 +18,12 @@ tests=( \
        test-cpu-mpich-py3_6-tf1_15_0-keras2_3_1-torch1_3_0-mxnet1_5_0-pyspark2_4_0 \
        test-cpu-oneccl-py3_6-tf1_15_0-keras2_3_1-torch1_3_0-mxnet1_5_0-pyspark2_4_0 \
        test-cpu-oneccl-ofi-py3_6-tf1_15_0-keras2_3_1-torch1_3_0-mxnet1_5_0-pyspark2_4_0 \
-       #test-gpu-openmpi-py3_6-tf1_15_0-keras2_3_1-torch1_3_0-mxnet1_4_1-pyspark2_4_0 \
-       #test-gpu-gloo-py3_6-tf1_15_0-keras2_3_1-torch1_3_0-mxnet1_4_1-pyspark2_4_0 \
-       #test-gpu-openmpi-gloo-py3_6-tf1_15_0-keras2_3_1-torch1_3_0-mxnet1_4_1-pyspark2_4_0 \
-       #test-gpu-openmpi-py3_6-tf2_0_0-keras2_3_1-torch1_3_0-mxnet1_5_0-pyspark2_4_0 \
-       #test-gpu-openmpi-py3_6-tfhead-kerashead-torchhead-mxnethead-pyspark2_4_0 \
-       #test-mixed-openmpi-py3_6-tf1_15_0-keras2_3_1-torch1_3_0-mxnet1_5_0-pyspark2_4_0 \
+       test-gpu-openmpi-py3_6-tf1_15_0-keras2_3_1-torch1_3_0-mxnet1_4_1-pyspark2_4_0 \
+       test-gpu-gloo-py3_6-tf1_15_0-keras2_3_1-torch1_3_0-mxnet1_4_1-pyspark2_4_0 \
+       test-gpu-openmpi-gloo-py3_6-tf1_15_0-keras2_3_1-torch1_3_0-mxnet1_4_1-pyspark2_4_0 \
+       test-gpu-openmpi-py3_6-tf2_0_0-keras2_3_1-torch1_3_0-mxnet1_5_0-pyspark2_4_0 \
+       test-gpu-openmpi-py3_6-tfhead-kerashead-torchhead-mxnethead-pyspark2_4_0 \
+       test-mixed-openmpi-py3_6-tf1_15_0-keras2_3_1-torch1_3_0-mxnet1_5_0-pyspark2_4_0 \
 )
 
 build_test() {
@@ -115,7 +115,7 @@ run_mpi_pytest() {
   # pytests have 4x GPU use cases and require a separate queue
   run_test "${test}" "${queue}" \
     ":pytest: Run PyTests (${test})" \
-    "bash -c \"${oneccl_env} cd /horovod/test && (echo test_*.py ${exclude_keras} ${excluded_tests} ${exclude_standalone_test} | xargs -n 1 \\\$(cat /mpirun_command) pytest -v --capture=no) && pytest --forked -v --capture=fd ${standalone_tests} | ts '[%Y-%m-%d %H:%M:%S]'\""
+    "bash -c \"${oneccl_env} cd /horovod/test && (echo test_*.py ${exclude_keras} ${excluded_tests} ${exclude_standalone_test} | xargs -n 1 \\\$(cat /mpirun_command) pytest -v --capture=no) && pytest --forked -v --capture=no ${standalone_tests}\""
 }
 
 run_mpi_integration() {
@@ -192,7 +192,7 @@ run_mpi() {
   local oneccl_env=${3:-}
 
   run_mpi_pytest ${test} ${queue} ${oneccl_env}
-  #run_mpi_integration ${test} ${queue} ${oneccl_env}
+  run_mpi_integration ${test} ${queue} ${oneccl_env}
 }
 
 run_gloo_pytest() {
@@ -222,7 +222,7 @@ run_gloo_pytest() {
 
   run_test "${test}" "${queue}" \
     ":pytest: Run PyTests (${test})" \
-    "bash -c \"cd /horovod/test && (echo test_*.py ${exclude_keras} ${excluded_tests} ${exclude_standalone_test} | xargs -n 1 horovodrun -np 2 -H localhost:2 --gloo pytest -v --capture=no) && pytest --forked -v --capture=fd ${standalone_tests} | ts '[%Y-%m-%d %H:%M:%S]'\""
+    "bash -c \"cd /horovod/test && (echo test_*.py ${exclude_keras} ${excluded_tests} ${exclude_standalone_test} | xargs -n 1 horovodrun -np 2 -H localhost:2 --gloo pytest -v --capture=no) && pytest --forked -v --capture=no ${standalone_tests}\""
 }
 
 run_gloo_integration() {
@@ -272,7 +272,7 @@ run_gloo() {
   local queue=$2
 
   run_gloo_pytest ${test} ${queue}
-  #run_gloo_integration ${test} ${queue}
+  run_gloo_integration ${test} ${queue}
 }
 
 run_spark_integration() {
@@ -391,5 +391,52 @@ for test in ${tests[@]}; do
       run_mpi ${test} "cpu" ${oneccl_env}
     fi
 
+    # always run spark tests which use MPI and Gloo
+    # TODO(travis): enable for Python 3.8 when Spark 3.0 released
+    #  see: https://issues.apache.org/jira/browse/SPARK-29536
+    if [[ ${test} != *"-py3_8-"* ]]; then
+        run_spark_integration ${test} "cpu"
+    fi
+
+    # no runner application, world size = 1
+    run_single_integration ${test} "cpu" ${oneccl_env}
+  fi
+done
+
+# wait for all cpu unit and integration tests to finish
+echo "- wait"
+
+# run 4x gpu unit tests
+for test in ${tests[@]}; do
+  if [[ ${test} == *-gpu-* ]] || [[ ${test} == *-mixed-* ]]; then
+    # if gloo is specified, run gloo gpu unit tests
+    if [[ ${test} == *-gloo* ]]; then
+      run_gloo_pytest ${test} "4x-gpu-g4"
+    fi
+
+    # if mpi is specified, run mpi gpu unit tests
+    if [[ ${test} == *mpi* ]]; then
+      run_mpi_pytest ${test} "4x-gpu-g4"
+    fi
+  fi
+done
+
+# wait for all gpu unit tests to finish
+echo "- wait"
+
+# run 2x gpu integration tests
+for test in ${tests[@]}; do
+  if [[ ${test} == *-gpu-* ]] || [[ ${test} == *-mixed-* ]]; then
+    # if gloo is specified, run gloo gpu integration tests
+    if [[ ${test} == *-gloo* ]]; then
+      run_gloo_integration ${test} "2x-gpu-g4"
+    fi
+
+    # if mpi is specified, run mpi gpu integration tests
+    if [[ ${test} == *mpi* ]]; then
+      run_mpi_integration ${test} "2x-gpu-g4"
+    fi
+
+    run_spark_integration ${test} "2x-gpu-g4"
   fi
 done
