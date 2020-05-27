740b07fd6df6075ef9d96ece6b8e68e135d0feb8
Alex Sergeev <alexander.sergeev@live.com>
2020-05-26 20:21:37 -0700

    Address review
    Signed-off-by: Alex Sergeev <alexander.sergeev@live.com>

--- horovod/torch/sync_batch_norm.py
+++ horovod/torch/sync_batch_norm.py
@@ -166,8 +166,9 @@ class _SyncBatchNorm(Function):
             sum_dy_xmu = synchronize(sum_dy_xmu_handle)
 
             if _SYNC_BN_V2 or _SYNC_BN_V3:
-                mean_dy = sum_dy / count_all.sum()
-                mean_dy_xmu = sum_dy_xmu / count_all.sum()
+                count_all_sum = count_all.sum()
+                mean_dy = sum_dy / count_all_sum
+                mean_dy_xmu = sum_dy_xmu / count_all_sum
             else:
                 # before 1.5.0, sum_dy was sum of means from every worker, so we just 
                 # need to divide it by number of workers
