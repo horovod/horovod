b562d641396814c082f38e8df839b1d3b20cc516
Travis Addair <taddair@uber.com>
2020-06-03 14:12:49 -0700

    Added Lightning examples
    Signed-off-by: Travis Addair <taddair@uber.com>

--- examples/pytorch_spark_mnist_legacy.py
+++ examples/pytorch_spark_mnist_legacy.py
@@ -1,24 +1,16 @@
 import argparse
 import os
 import subprocess
-from distutils.version import LooseVersion
 
 import numpy as np
 
-import pyspark
 import pyspark.sql.types as T
 from pyspark import SparkConf
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-if LooseVersion(pyspark.__version__) < LooseVersion('3.0.0'):
-    from pyspark.ml.feature import OneHotEncoderEstimator as OneHotEncoder
-else:
-    from pyspark.ml.feature import OneHotEncoder
+from pyspark.ml.feature import OneHotEncoderEstimator
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
 
-from pytorch_lightning import LightningModule
-
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
@@ -26,7 +18,7 @@ import torch.optim as optim
 import horovod.spark.torch as hvd
 from horovod.spark.common.store import Store
 
-parser = argparse.ArgumentParser(description='PyTorch Spark MNIST Example',
+parser = argparse.ArgumentParser(description='Keras Spark MNIST Example',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--master',
                     help='spark master to connect to')
@@ -45,7 +37,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Initialize SparkSession
-    conf = SparkConf().setAppName('pytorch_spark_mnist').set('spark.sql.shuffle.partitions', '16')
+    conf = SparkConf().setAppName('keras_spark_mnist').set('spark.sql.shuffle.partitions', '16')
     if args.master:
         conf.setMaster(args.master)
     elif args.num_proc:
@@ -67,9 +59,9 @@ if __name__ == '__main__':
         .load(libsvm_path)
 
     # One-hot encode labels into SparseVectors
-    encoder = OneHotEncoder(inputCols=['label'],
-                            outputCols=['label_vec'],
-                            dropLast=False)
+    encoder = OneHotEncoderEstimator(inputCols=['label'],
+                                     outputCols=['label_vec'],
+                                     dropLast=False)
     model = encoder.fit(df)
     train_df = model.transform(df)
 
@@ -77,7 +69,7 @@ if __name__ == '__main__':
     train_df, test_df = train_df.randomSplit([0.9, 0.1])
 
     # Define the PyTorch model without any Horovod-specific parameters
-    class Net(LightningModule):
+    class Net(nn.Module):
         def __init__(self):
             super(Net, self).__init__()
             self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
@@ -96,33 +88,17 @@ if __name__ == '__main__':
             x = self.fc2(x)
             return F.log_softmax(x)
 
-        def configure_optimizers(self):
-            return optim.SGD(self.parameters(), lr=0.01, momentum=0.5)
-
-        def training_step(self, batch, batch_nb):
-            x, y = batch
-            y_hat = self(x)
-            loss = F.nll_loss(y_hat, y)
-            tensorboard_logs = {'train_loss': loss}
-            return {'loss': loss, 'log': tensorboard_logs}
-
-        def validation_step(self, batch, batch_nb):
-            x, y = batch
-            y_hat = self(x)
-            return {'val_loss': F.nll_loss(y_hat, y)}
-
-        def validation_epoch_end(self, outputs):
-            avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-            tensorboard_logs = {'val_loss': avg_loss}
-            return {'avg_val_loss': avg_loss, 'log': tensorboard_logs}
-
 
     model = Net()
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+    loss = nn.NLLLoss()
 
     # Train a Horovod Spark Estimator on the DataFrame
     torch_estimator = hvd.TorchEstimator(num_proc=args.num_proc,
                                          store=store,
                                          model=model,
+                                         optimizer=optimizer,
+                                         loss=lambda input, target: loss(input, target.long()),
                                          input_shapes=[[-1, 1, 28, 28]],
                                          feature_cols=['features'],
                                          label_cols=['label'],
