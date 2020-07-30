3e47eb76bb192a88a7e5cc546304307004c4fa57
Fardin Abdi <fardin@uber.com>
2020-07-29 18:42:09 -0700

    change loose version comparison
    Signed-off-by: Fardin Abdi <fardin@uber.com>

--- test/test_spark_keras.py
+++ test/test_spark_keras.py
@@ -139,9 +139,6 @@ class SparkKerasTests(tf.test.TestCase):
                     label_prob = row.label_prob.toArray().tolist()
                     assert label_prob[int(row.label_pred)] == max(label_prob)
 
-    @pytest.mark.skipif(LooseVersion(tf.__version__) == '1.14.0',
-                        reason='This test segfaults with Tensorflow 1.14.0: '
-                               'https://github.com/horovod/horovod/issues/1995')
     @mock.patch('horovod.spark.keras.remote._pin_gpu_fn')
     @mock.patch('horovod.spark.keras.util.TFKerasUtil.fit_fn')
     def test_restore_from_checkpoint(self, mock_fit_fn, mock_pin_gpu_fn):
@@ -189,9 +186,6 @@ class SparkKerasTests(tf.test.TestCase):
                 keras_estimator.fit(df)
                 keras_estimator._load_model_from_checkpoint.assert_called()
 
-    @pytest.mark.skipif(LooseVersion(tf.__version__) == '1.14.0',
-                        reason='This test segfaults with Tensorflow 1.14.0: '
-                               'https://github.com/horovod/horovod/issues/1995')
     @mock.patch('horovod.spark.keras.remote._pin_gpu_fn')
     @mock.patch('horovod.spark.keras.util.TFKerasUtil.fit_fn')
     def test_keras_direct_parquet_train(self, mock_fit_fn, mock_pin_gpu_fn):
@@ -231,7 +225,7 @@ class SparkKerasTests(tf.test.TestCase):
                     predictions = transformer.transform(df)
                 assert predictions.count() == df.count()
 
-    @pytest.mark.skipif(LooseVersion(tf.__version__) == '1.14.0',
+    @pytest.mark.skipif(LooseVersion(tf.__version__) == LooseVersion('1.14.0'),
                         reason='This test segfaults with Tensorflow 1.14.0: '
                                'https://github.com/horovod/horovod/issues/1995')
     @mock.patch('horovod.spark.keras.remote._pin_gpu_fn')
