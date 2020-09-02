08902f7701ecf821768a3b1a502b50481c5ed3de
Travis Addair <taddair@uber.com>
2020-09-02 05:44:38 -0700

    Updated Dockerfile packages and fixed issues with wheel (#2223)
    Signed-off-by: Travis Addair <taddair@uber.com>

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -9,8 +9,13 @@ function build_one()
     device=$2
 
     tensorflow_pkg='tensorflow'
+    if [[ ${device} == 'cpu' ]]; then
+        tensorflow_pkg='tensorflow-cpu'
+    fi
+
+    mxnet_pkg='mxnet'
     if [[ ${device} == 'gpu' ]]; then
-        tensorflow_pkg='tensorflow-gpu'
+        mxnet_pkg='mxnet-cu101'
     fi
 
     tag=horovod-build-py${py}-${device}:$(date +%Y%m%d-%H%M%S)
@@ -18,19 +23,19 @@ function build_one()
     horovod_version=$(docker run --rm ${tag} pip show horovod | grep Version | awk '{print $2}')
     tensorflow_version=$(docker run --rm ${tag} pip show ${tensorflow_pkg} | grep Version | awk '{print $2}')
     pytorch_version=$(docker run --rm ${tag} pip show torch | grep Version | awk '{print $2}')
-    mxnet_version=$(docker run --rm ${tag} pip show mxnet | grep Version | awk '{print $2}')
+    mxnet_version=$(docker run --rm ${tag} pip show ${mxnet_pkg} | grep Version | awk '{print $2}')
     final_tag=horovod/horovod:${horovod_version}-tf${tensorflow_version}-torch${pytorch_version}-mxnet${mxnet_version}-py${py}-${device}
     docker tag ${tag} ${final_tag}
     docker rmi ${tag}
 }
 
 # clear upstream images, ok to fail if images do not exist
-docker rmi $(cat Dockerfile.gpu | grep FROM | awk '{print $2}') || true
 docker rmi $(cat Dockerfile.cpu | grep FROM | awk '{print $2}') || true
+docker rmi $(cat Dockerfile.gpu | grep FROM | awk '{print $2}') || true
 
 # build for cpu and gpu
-build_one 3.6 gpu
-build_one 3.6 cpu
+build_one 3.7 cpu
+build_one 3.7 gpu
 
 # print recent images
 docker images horovod/horovod
