876285ab9ee378b39308c1d68f493e3898d80c62
Richard Liaw <rliaw@berkeley.edu>
2020-09-22 13:55:08 -0700

    fix-example
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- examples/elastic/pytorch_ray_elastic.py
+++ examples/elastic/pytorch_ray_elastic.py
@@ -9,28 +9,97 @@ import os
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                    help='input batch size for training (default: 64)')
-parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                    help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=5, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                    help='learning rate (default: 0.01)')
-parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-                    help='SGD momentum (default: 0.5)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='disables CUDA training')
-parser.add_argument('--seed', type=int, default=42, metavar='S',
-                    help='random seed (default: 42)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
-parser.add_argument('--fp16-allreduce', action='store_true', default=False,
-                    help='use fp16 compression during allreduce')
-parser.add_argument('--use-adasum', action='store_true', default=False,
-                    help='use adasum algorithm to do reduction')
+parser.add_argument(
+    '--batch-size',
+    type=int,
+    default=64,
+    metavar='N',
+    help='input batch size for training (default: 64)')
+parser.add_argument(
+    '--test-batch-size',
+    type=int,
+    default=1000,
+    metavar='N',
+    help='input batch size for testing (default: 1000)')
+parser.add_argument(
+    '--epochs',
+    type=int,
+    default=5,
+    metavar='N',
+    help='number of epochs to train (default: 10)')
+parser.add_argument(
+    '--lr',
+    type=float,
+    default=0.01,
+    metavar='LR',
+    help='learning rate (default: 0.01)')
+parser.add_argument(
+    '--momentum',
+    type=float,
+    default=0.5,
+    metavar='M',
+    help='SGD momentum (default: 0.5)')
+parser.add_argument(
+    '--no-cuda',
+    action='store_true',
+    default=False,
+    help='disables CUDA training')
+parser.add_argument(
+    '--seed',
+    type=int,
+    default=42,
+    metavar='S',
+    help='random seed (default: 42)')
+parser.add_argument(
+    '--log-interval',
+    type=int,
+    default=10,
+    metavar='N',
+    help='how many batches to wait before logging training status')
+parser.add_argument(
+    '--fp16-allreduce',
+    action='store_true',
+    default=False,
+    help='use fp16 compression during allreduce')
+parser.add_argument(
+    '--use-adasum',
+    action='store_true',
+    default=False,
+    help='use adasum algorithm to do reduction')
 
 args = parser.parse_args()
+
+
+def metric_average(val, name):
+    tensor = torch.tensor(val)
+    avg_tensor = hvd.allreduce(tensor, name=name)
+    return avg_tensor.item()
+
+
+def check_rank(epoch):
+    if epoch == 2 and int(os.environ.get('HOROVOD_RANK')) == 0:
+        print('exit rank {}'.format(hvd.rank()))
+        raise RuntimeError('check_rank and exit')
+        # exit(1)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
 
 
 def train_fn():
@@ -43,7 +112,6 @@ def train_fn():
         # Horovod: pin GPU to local rank.
         torch.cuda.set_device(hvd.local_rank())
         torch.cuda.manual_seed(args.seed)
-
 
     # Horovod: limit # of CPU threads to be used per worker.
     torch.set_num_threads(1)
@@ -59,38 +127,24 @@ def train_fn():
     train_sampler = torch.utils.data.distributed.DistributedSampler(
         train_dataset, num_replicas=hvd.size(), rank=hvd.rank())
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=args.batch_size, sampler=train_sampler, **kwargs)
+        train_dataset,
+        batch_size=args.batch_size,
+        sampler=train_sampler,
+        **kwargs)
+    transformations = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.1307, ), (0.3081, ))])
 
-    test_dataset = \
-        datasets.MNIST('data-%d' % hvd.rank(), train=False, transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ]))
+    test_dataset = datasets.MNIST(
+        'data-%d' % hvd.rank(), train=False, transforms=transformations)
     # Horovod: use DistributedSampler to partition the test data.
     test_sampler = torch.utils.data.distributed.DistributedSampler(
         test_dataset, num_replicas=hvd.size(), rank=hvd.rank())
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.test_batch_size,
-                                              sampler=test_sampler, **kwargs)
-
-
-    class Net(nn.Module):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-            self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-            self.conv2_drop = nn.Dropout2d()
-            self.fc1 = nn.Linear(320, 50)
-            self.fc2 = nn.Linear(50, 10)
-
-        def forward(self, x):
-            x = F.relu(F.max_pool2d(self.conv1(x), 2))
-            x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-            x = x.view(-1, 320)
-            x = F.relu(self.fc1(x))
-            x = F.dropout(x, training=self.training)
-            x = self.fc2(x)
-            return F.log_softmax(x)
-
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=args.test_batch_size,
+        sampler=test_sampler,
+        **kwargs)
 
     model = Net()
 
@@ -105,25 +159,12 @@ def train_fn():
             lr_scaler = hvd.local_size()
 
     # Horovod: scale learning rate by lr_scaler.
-    optimizer = optim.SGD(model.parameters(), lr=args.lr * lr_scaler,
-                          momentum=args.momentum)
+    optimizer = optim.SGD(
+        model.parameters(), lr=args.lr * lr_scaler, momentum=args.momentum)
 
     # Horovod: (optional) compression algorithm.
-    compression = hvd.Compression.fp16 if args.fp16_allreduce else hvd.Compression.none
-
-
-    def metric_average(val, name):
-        tensor = torch.tensor(val)
-        avg_tensor = hvd.allreduce(tensor, name=name)
-        return avg_tensor.item()
-
-
-    def check_rank(epoch):
-        if epoch == 2 and int(os.environ.get('HOROVOD_RANK')) == 0:
-            print('exit rank {}'.format(hvd.rank()))
-            raise RuntimeError('check_rank and exit')
-            # exit(1)
-
+    compression = (hvd.Compression.fp16
+                   if args.fp16_allreduce else hvd.Compression.none)
 
     @hvd.elastic.run
     def train(state):
@@ -147,14 +188,15 @@ def train_fn():
                 loss.backward()
                 state.optimizer.step()
                 if state.batch % args.log_interval == 0:
-                    # Horovod: use train_sampler to determine the number of examples in
-                    # this worker's partition.
-                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                        state.epoch, state.batch * len(data), len(train_sampler),
-                        100.0 * state.batch / len(train_loader), loss.item()))
+                    # Horovod: use train_sampler to determine
+                    # the number of examples in this worker's partition.
+                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.
+                          format(state.epoch, state.batch * len(data),
+                                 len(train_sampler),
+                                 100.0 * state.batch / len(train_loader),
+                                 loss.item()))
                 state.commit()
             state.batch = 0
-
 
     def test():
         model.eval()
@@ -168,7 +210,8 @@ def train_fn():
             test_loss += F.nll_loss(output, target, size_average=False).item()
             # get the index of the max log-probability
             pred = output.data.max(1, keepdim=True)[1]
-            test_accuracy += pred.eq(target.data.view_as(pred)).cpu().float().sum()
+            test_accuracy += pred.eq(
+                target.data.view_as(pred)).cpu().float().sum()
 
         # Horovod: use test_sampler to determine the number of examples in
         # this worker's partition.
@@ -181,27 +224,27 @@ def train_fn():
 
         # Horovod: print output only on first rank.
         if hvd.rank() == 0:
-            print('\nTest set: Average loss: {:.4f}, Accuracy: {:.2f}%\n'.format(
-                test_loss, 100. * test_accuracy))
-
+            print(
+                '\nTest set: Average loss: {:.4f}, Accuracy: {:.2f}%\n'.format(
+                    test_loss, 100. * test_accuracy))
 
     # Horovod: wrap optimizer with DistributedOptimizer.
-    optimizer = hvd.DistributedOptimizer(optimizer,
-                                         named_parameters=model.named_parameters(),
-                                         compression=compression,
-                                         op=hvd.Adasum if args.use_adasum else hvd.Average)
-
+    optimizer = hvd.DistributedOptimizer(
+        optimizer,
+        named_parameters=model.named_parameters(),
+        compression=compression,
+        op=hvd.Adasum if args.use_adasum else hvd.Average)
 
     # adjust learning rate on reset
     def on_state_reset():
         for param_group in optimizer.param_groups:
             param_group['lr'] = args.lr * hvd.size()
 
-
     state = hvd.elastic.TorchState(model, optimizer, epoch=1, batch=0)
     state.register_reset_callbacks([on_state_reset])
     train(state)
     test()
+
 
 if __name__ == '__main__':
     from horovod.ray.elastic import ElasticRayExecutor
