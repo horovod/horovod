5e180360211ca54c389d5fbf2109d0e50a3d8864
aaron276h <aaron@determined.ai>
2020-10-02 16:41:57 -0400

    Add defaults for creating keras optimizers
    Signed-off-by: aaron276h <aaron@determined.ai>

--- horovod/_keras/__init__.py
+++ horovod/_keras/__init__.py
@@ -26,7 +26,7 @@ _PRE_TF_2_4_0 = LooseVersion(tf.__version__) < LooseVersion('2.4.0')
 
 def create_distributed_optimizer(keras, optimizer, name, device_dense, device_sparse,
                                  compression, sparse_as_dense, gradient_predivide_factor,
-                                 backward_passes_per_step, average_aggregated_gradients):
+                                 backward_passes_per_step=1, average_aggregated_gradients=True):
     class _DistributedOptimizer(keras.optimizers.Optimizer):
         _HAS_AGGREGATE_GRAD = True
 
