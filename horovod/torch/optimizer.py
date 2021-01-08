Fixed sparse gradient aggregation when different ranks submit different numbers of elements
Signed-off-by: Travis Addair <tgaddair@gmail.com>

horovod/torch/optimizer.py
@@ -186,8 +186,11 @@ class _DistributedOptimizer(torch.optim.Optimizer):
         return handle, ctxs
 
     def _sparse_allreduce_grad_async(self, p, name):
+        # Allgather aggregates along the first dimension, so we need to transpose the
+        # indices to enforce correct concatenation behavior, then transpose back prior to
+        # constructing the new aggregated sparse gradient
         grad = p.grad.coalesce()
-        indices_handle = allgather_async(grad.indices(), name=f'{name}.indices')
+        indices_handle = allgather_async(grad.indices().transpose(0, 1), name=f'{name}.indices')
         values_handle = allgather_async(grad.values(), name=f'{name}.values')
 
         def handle():
@@ -195,10 +198,9 @@ class _DistributedOptimizer(torch.optim.Optimizer):
             values = synchronize(values_handle)
             values = (values / size()) if self.op == Average else values
 
-            constructor = grad.new
             if indices.dim() == 0 or values.dim() == 0:
-                return constructor().resize_as_(grad)
-            return constructor(indices.reshape([1, -1]), values, grad.size())
+                return grad.new().resize_as_(grad)
+            return grad.new(indices.transpose(0, 1), values, grad.size())
 
         return handle, None
 
@@ -260,7 +262,10 @@ class _DistributedOptimizer(torch.optim.Optimizer):
                 if p.grad.is_sparse:
                     aggregated = self._compression.decompress(output, ctx)
                     if not aggregated.is_sparse:
+                        # When sparse_as_dense=True we need to convert the grad back to sparse before update
                         aggregated = aggregated.to_sparse()
+
+                    # Sparse grads do not support set_ for some reason, so we do this as an equivalent
                     p.grad.zero_().add_(aggregated)
                 else:
                     p.grad.set_(self._compression.decompress(output, ctx))

test/parallel/test_torch.py
@@ -2346,7 +2346,11 @@ class TorchTests(unittest.TestCase):
                     return x
 
             model = Net()
-            inp = torch.LongTensor([[1, 2, 4, 5], [4, 3, 2, 9]])
+
+            if hvd.rank() == 0:
+                inp = torch.LongTensor([[1, 2, 4, 5], [4, 3, 2, 9]])
+            else:
+                inp = torch.LongTensor([[1, 3, 4], [4, 7, 9]])
 
             # list() see: https://github.com/pytorch/pytorch/issues/47594
             opt = torch.optim.SparseAdam(list(model.parameters()), lr=0.1)