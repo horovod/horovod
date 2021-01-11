68f2cae5e53074539fb2c8b921198a81efa883b8
Travis Addair <tgaddair@gmail.com>
2021-01-11 13:50:48 -0800

    Removed unused
    Signed-off-by: Travis Addair <tgaddair@gmail.com>

--- examples/pytorch/pytorch_cifar10.py
+++ examples/pytorch/pytorch_cifar10.py
@@ -259,14 +259,6 @@ class ResNet(nn.Module):
         return out
 
 
-def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
-
-
-def ResNet34():
-    return ResNet(BasicBlock, [3, 4, 6, 3])
-
-
 def ResNet50():
     return ResNet(Bottleneck, [3, 4, 6, 3])
 
