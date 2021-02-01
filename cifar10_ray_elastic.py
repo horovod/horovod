7b7c15412f012918327cc95ae1d49a33aad0524f
Richard Liaw <rliaw@berkeley.edu>
2021-02-01 00:57:10 -0800

    yapf
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- cifar10_ray_elastic.py
+++ cifar10_ray_elastic.py
@@ -20,45 +20,68 @@ from horovod.torch.elastic.sampler import ElasticSampler
 from horovod.ray.elastic import RayHostDiscovery
 
 # Training settings
-parser = argparse.ArgumentParser(description='PyTorch Cifar10 Example',
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--log-dir', default='./logs',
-                    help='tensorboard log directory')
-parser.add_argument('--checkpoint-format', default='./checkpoint-{epoch}.pth.tar',
-                    help='checkpoint file format')
-parser.add_argument('--data-dir', default='./new_data',
-                    help='cifar10 dataset directory')
+parser = argparse.ArgumentParser(
+    description='PyTorch Cifar10 Example',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    '--log-dir', default='./logs', help='tensorboard log directory')
+parser.add_argument(
+    '--checkpoint-format',
+    default='./checkpoint-{epoch}.pth.tar',
+    help='checkpoint file format')
+parser.add_argument(
+    '--data-dir', default='./new_data', help='cifar10 dataset directory')
 
-parser.add_argument('--epochs', type=int, default=90,
-                    help='number of epochs to train')
-parser.add_argument('--lr', type=float, default=0.01,
-                    help='learning rate for a single GPU')
+parser.add_argument(
+    '--epochs', type=int, default=90, help='number of epochs to train')
+parser.add_argument(
+    '--lr', type=float, default=0.01, help='learning rate for a single GPU')
 
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='disables CUDA training')
-parser.add_argument('--seed', type=int, default=42,
-                    help='random seed')
-parser.add_argument('--change-frequency-s', type=int, default=40,
-                    help='random seed')
+parser.add_argument(
+    '--no-cuda',
+    action='store_true',
+    default=False,
+    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=42, help='random seed')
+parser.add_argument(
+    '--change-frequency-s', type=int, default=40, help='random seed')
 
 # Elastic Horovod settings
-parser.add_argument('--batches-per-commit', type=int, default=50,
-                    help='number of batches processed before calling `state.commit()`; '
-                         'commits prevent losing progress if an error occurs, but slow '
-                         'down training.')
-parser.add_argument('--batches-per-host-check', type=int, default=10,
-                    help='number of batches processed before calling `state.check_host_updates()`; '
-                         'this check is very fast compared to state.commit() (which calls this '
-                         'as part of the commit process), but because still incurs some cost due '
-                         'to broadcast, so we may not want to perform it every batch.')
+parser.add_argument(
+    '--batches-per-commit',
+    type=int,
+    default=50,
+    help='number of batches processed before calling `state.commit()`; '
+    'commits prevent losing progress if an error occurs, but slow '
+    'down training.')
+parser.add_argument(
+    '--batches-per-host-check',
+    type=int,
+    default=10,
+    help=
+    'number of batches processed before calling `state.check_host_updates()`; '
+    'this check is very fast compared to state.commit() (which calls this '
+    'as part of the commit process), but because still incurs some cost due '
+    'to broadcast, so we may not want to perform it every batch.')
 
 args = parser.parse_args()
 
 
 class TestDiscovery(RayHostDiscovery):
-    def __init__(self, min_hosts, max_hosts, change_frequency_s, use_gpu=False, cpus_per_slot=1, gpus_per_slot=1):
-        super().__init__(use_gpu=use_gpu, cpus_per_slot=cpus_per_slot, gpus_per_slot=gpus_per_slot)
+    def __init__(self,
+                 min_hosts,
+                 max_hosts,
+                 change_frequency_s,
+                 use_gpu=False,
+                 cpus_per_slot=1,
+                 gpus_per_slot=1,
+                 hard_kill=False):
+        super().__init__(
+            use_gpu=use_gpu,
+            cpus_per_slot=cpus_per_slot,
+            gpus_per_slot=gpus_per_slot)
         self._min_hosts = min_hosts
+        self._hard_kill = hard_kill
         self._max_hosts = max_hosts
         self._change_frequency_s = change_frequency_s
         self._last_reset_t = None
@@ -75,13 +98,18 @@ class TestDiscovery(RayHostDiscovery):
             print("No hosts to add.")
 
     def remove_host(self, hosts):
-        from ray.autoscaler._private.commands import kill_node
-        # host = kill_node(os.path.expanduser("~/ray_bootstrap_config.yaml"), True, False, None)
         good_hosts = [k for k in hosts if k not in self._removed_hosts]
+
+        from ray.autoscaler._private.commands import kill_node
         if good_hosts:
-            host = random.choice(good_hosts)
-            print('REMOVE HOST', host, hosts, self._removed_hosts)
-            self._removed_hosts.add(host)
+            if self._hard_kill:
+                host = kill_node(
+                    os.path.expanduser("~/ray_bootstrap_config.yaml"), True,
+                    False, None)
+            else:
+                host = random.choice(good_hosts)
+        print('REMOVE HOST', host, hosts, self._removed_hosts)
+        self._removed_hosts.add(host)
 
     def change_hosts(self, hosts):
         for host in self._removed_hosts:
@@ -107,7 +135,10 @@ class TestDiscovery(RayHostDiscovery):
             self.change_hosts(hosts)
             self._last_reset_t = t
         print(f"Total hosts: {len(hosts)}")
-        remaining = {k: v for k, v in hosts.items() if k not in self._removed_hosts}
+        remaining = {
+            k: v
+            for k, v in hosts.items() if k not in self._removed_hosts
+        }
         print(f"Remaining hosts: {len(remaining)} -- {remaining}")
         return remaining
 
@@ -127,27 +158,34 @@ def load_data():
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010)),
     ])
 
     transform_test = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010)),
     ])
     from filelock import FileLock
     with FileLock(os.path.expanduser("~/.datalock")):
         train_dataset = datasets.CIFAR10(
-            root=args.data_dir, train=True, download=True, transform=transform_train)
+            root=args.data_dir,
+            train=True,
+            download=True,
+            transform=transform_train)
     train_sampler = ElasticSampler(train_dataset)
     train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=4,
-        sampler=train_sampler, **kwargs)
+        train_dataset, batch_size=64, sampler=train_sampler, **kwargs)
 
     val_dataset = datasets.CIFAR10(
-        root=args.data_dir, train=False, download=True, transform=transform_test)
+        root=args.data_dir,
+        train=False,
+        download=True,
+        transform=transform_test)
     val_sampler = ElasticSampler(val_dataset)
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=4,
-                                             sampler=val_sampler, **kwargs)
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=64, sampler=val_sampler, **kwargs)
 
     return train_loader, val_loader, train_sampler, val_sampler
 
@@ -161,9 +199,10 @@ def train(state, train_loader, log_writer, verbose):
     train_loss = Metric('train_loss')
     train_accuracy = Metric('train_accuracy')
 
-    with tqdm(total=len(train_loader),
-              desc='Train Epoch     #{}'.format(epoch + 1),
-              disable=not verbose) as t:
+    with tqdm(
+            total=len(train_loader),
+            desc='Train Epoch     #{}'.format(epoch + 1),
+            disable=not verbose) as t:
 
         for batch_idx, (data, target) in enumerate(train_loader):
             # Elastic Horovod: update the current batch index this epoch
@@ -189,8 +228,10 @@ def train(state, train_loader, log_writer, verbose):
             loss.backward()
             state.optimizer.step()
 
-            t.set_postfix({'loss': train_loss.avg.item(),
-                           'accuracy': 100. * train_accuracy.avg.item()})
+            t.set_postfix({
+                'loss': train_loss.avg.item(),
+                'accuracy': 100. * train_accuracy.avg.item()
+            })
             t.update(1)
 
     if log_writer:
@@ -203,9 +244,10 @@ def validate(state, val_loader, log_writer, verbose):
     val_loss = Metric('val_loss')
     val_accuracy = Metric('val_accuracy')
 
-    with tqdm(total=len(val_loader),
-              desc='Validate Epoch  #{}'.format(state.epoch + 1),
-              disable=not verbose) as t:
+    with tqdm(
+            total=len(val_loader),
+            desc='Validate Epoch  #{}'.format(state.epoch + 1),
+            disable=not verbose) as t:
         with torch.no_grad():
             for data, target in val_loader:
                 if args.cuda:
@@ -214,8 +256,10 @@ def validate(state, val_loader, log_writer, verbose):
 
                 val_loss.update(F.cross_entropy(output, target))
                 val_accuracy.update(accuracy(output, target))
-                t.set_postfix({'loss': val_loss.avg.item(),
-                               'accuracy': 100. * val_accuracy.avg.item()})
+                t.set_postfix({
+                    'loss': val_loss.avg.item(),
+                    'accuracy': 100. * val_accuracy.avg.item()
+                })
                 t.update(1)
 
     if log_writer:
@@ -270,19 +314,26 @@ class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+            in_planes,
+            planes,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(
+            planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
+        if stride != 1 or in_planes != self.expansion * planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
+                nn.Conv2d(
+                    in_planes,
+                    self.expansion * planes,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False), nn.BatchNorm2d(self.expansion * planes))
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
@@ -299,20 +350,27 @@ class Bottleneck(nn.Module):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=stride, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(
+            planes,
+            planes,
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion *
-                               planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+        self.conv3 = nn.Conv2d(
+            planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
 
         self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
+        if stride != 1 or in_planes != self.expansion * planes:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
+                nn.Conv2d(
+                    in_planes,
+                    self.expansion * planes,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False), nn.BatchNorm2d(self.expansion * planes))
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
@@ -328,17 +386,17 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(
+            3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.Linear(512 * block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
+        strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
             layers.append(block(self.in_planes, planes, stride))
@@ -390,16 +448,18 @@ def run():
         model.cuda()
 
     # Horovod: scale learning rate by the number of GPUs.
-    optimizer = optim.SGD(model.parameters(),
-                          lr=args.lr * np.sqrt(hvd.size()),
-                          momentum=0.9, weight_decay=5e-4)
+    optimizer = optim.SGD(
+        model.parameters(),
+        lr=args.lr * np.sqrt(hvd.size()),
+        momentum=0.9,
+        weight_decay=5e-4)
 
     # Horovod: wrap optimizer with DistributedOptimizer.
     optimizer = hvd.DistributedOptimizer(
-        optimizer, named_parameters=model.named_parameters()
-    )
+        optimizer, named_parameters=model.named_parameters())
 
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, T_max=200)
 
     # Restore from a previous checkpoint, if initial_epoch is specified.
     # Horovod: restore on the first worker which will broadcast weights to other workers.
@@ -453,8 +513,8 @@ if __name__ == '__main__':
         max_hosts=5,
         change_frequency_s=args.change_frequency_s,
         use_gpu=True,
-        cpus_per_slot=1
-    )
-    executor = ElasticRayExecutor(settings, use_gpu=True, cpus_per_slot=1, override_discovery=False)
+        cpus_per_slot=1)
+    executor = ElasticRayExecutor(
+        settings, use_gpu=True, cpus_per_slot=1, override_discovery=False)
     executor.start()
     executor.run(run)
