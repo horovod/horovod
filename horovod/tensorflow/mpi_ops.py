Fix gradients for allgather() and broadcast() (#2654).
Also, fix corresponding tests.

Signed-off-by: Loïc Ehrhardt <loic.ehrhardt@gmail.com>

horovod/tensorflow/mpi_ops.py
@@ -219,7 +219,7 @@ def _allgather_grad(op, grad):
       The gradient with respect to the input of the op.
     """
     ignore_name_scope = op.get_attr('ignore_name_scope')
-    grad = _allreduce(grad, ignore_name_scope=ignore_name_scope)
+    grad = _allreduce(grad, op=Average, ignore_name_scope=ignore_name_scope)
 
     with tf.device('/cpu:0'):
         # Keep the tensor of split sizes on CPU.
@@ -265,7 +265,8 @@ def _broadcast_grad(op, grad):
     """
     root_rank = op.get_attr('root_rank')
     ignore_name_scope = op.get_attr('ignore_name_scope')
-    grad_reduced = _allreduce(grad, ignore_name_scope=ignore_name_scope)
+    grad_reduced = _allreduce(grad, op=Average,
+                              ignore_name_scope=ignore_name_scope)
     if rank() != root_rank:
         return grad_reduced * 0
     return grad_reduced

horovod/torch/mpi_ops.py
@@ -554,7 +554,7 @@ class HorovodAllgather(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
-        grad_reduced = allreduce(grad_output, average=False)
+        grad_reduced = allreduce(grad_output, average=True)
 
         dim_t = torch.IntTensor([ctx.dim])
         dim = allgather(dim_t).view(size())
@@ -639,7 +639,7 @@ class HorovodBroadcast(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
-        grad_reduced = allreduce(grad_output, average=False)
+        grad_reduced = allreduce(grad_output, average=True)
         if rank() != ctx.root_rank:
             grad_reduced *= 0
         return grad_reduced, None, None