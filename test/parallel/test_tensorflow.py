Fix gradients for allgather() and broadcast() (#2654). (#2687)
Also, fix corresponding tests.

Signed-off-by: Loïc Ehrhardt <loic.ehrhardt@gmail.com>

test/parallel/test_tensorflow.py
@@ -1471,7 +1471,7 @@ class TensorFlowTests(tf.test.TestCase):
 
             expected = np.ones(
                 [tensor_sizes[rank]] + [17] * (dim - 1)
-            ) * rank * size
+            ) * rank
             err = np.linalg.norm(expected - grad_out)
             self.assertLess(err, 0.00000001,
                             "gradient %s differs from expected %s, "
@@ -1534,7 +1534,7 @@ class TensorFlowTests(tf.test.TestCase):
 
             expected = np.ones(
                 [tensor_sizes[rank]] + [17] * (dim - 1)
-            ) * rank * size
+            ) * rank
             err = np.linalg.norm(expected - grad_out)
             self.assertLess(err, 0.00000001,
                             "gradient %s differs from expected %s, "
@@ -1694,7 +1694,7 @@ class TensorFlowTests(tf.test.TestCase):
                     grad = tf.gradients(broadcasted_tensor, tensor, grad_ys)[0]
                     grad_out = self.evaluate(grad)
 
-            c = size if rank == root_rank else 0
+            c = 1 if rank == root_rank else 0
             expected = np.ones([5] * dim) * c
             err = np.linalg.norm(expected - grad_out)
             self.assertLess(err, 0.00000001,
@@ -1745,7 +1745,7 @@ class TensorFlowTests(tf.test.TestCase):
                     grad = tf.gradients(broadcasted_tensor, tensor, grad_ys)[0]
                     grad_out = self.evaluate(grad)
 
-            c = size if rank == root_rank else 0
+            c = 1 if rank == root_rank else 0
             expected = np.ones([5] * dim) * c
             err = np.linalg.norm(expected - grad_out)
             self.assertLess(err, 0.00000001,

test/parallel/test_torch.py
@@ -988,7 +988,7 @@ class TorchTests(unittest.TestCase):
 
             expected = np.ones(
                 [tensor_sizes[rank]] + [17] * (dim - 1)
-            ) * rank * size
+            ) * rank
             err = np.linalg.norm(expected - grad_out)
             self.assertLess(err, 0.00000001,
                             "gradient %s differs from expected %s, "
@@ -1170,7 +1170,7 @@ class TorchTests(unittest.TestCase):
             broadcasted_tensor.backward(self.cast_and_place(torch.ones([17] * dim), dtype))
             grad_out = tensor.grad.data.cpu().numpy()
 
-            c = size if rank == root_rank else 0
+            c = 1 if rank == root_rank else 0
             expected = np.ones([17] * dim) * c
             err = np.linalg.norm(expected - grad_out)
             self.assertLess(err, 0.00000001,