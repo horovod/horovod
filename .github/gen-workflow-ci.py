7daabba2db56ac7b5a0ebf19bed7e75a425ff51e
Enrico Minack <github@enrico.minack.dev>
2021-03-29 17:44:26 +0200

    Set parallelism to number of images
    Signed-off-by: Enrico Minack <github@enrico.minack.dev>

--- .github/gen-workflow-ci.py
+++ .github/gen-workflow-ci.py
@@ -100,6 +100,7 @@ def main():
               f'    runs-on: ubuntu-18.04\n'
               f'    continue-on-error: true\n'
               f'    strategy:\n'
+              f'      max-parallel: {len(images)}\n'
               f'      fail-fast: false\n'
               f'      matrix:\n'
               f'        include:\n' +
