606a6fb2d2bf73c0fd5e36f8b00bb0d96142fd69
Trent Lo <trentl@nvidia.com>
2021-07-14 17:07:14 -0700

    Polish codes.
    Signed-off-by: Trent Lo <trentl@nvidia.com>

--- horovod/tensorflow/xla_mpi_ops.cc
+++ horovod/tensorflow/xla_mpi_ops.cc
@@ -42,8 +42,6 @@
         << "CUDA: " << cudaGetErrorString(e);                                  \
   }
 
-#endif // HAVE_GPU
-
 #define OMPI_SKIP_MPICXX
 #include "../common/operations.h"
 #include "../common/utils/env_parser.h"
@@ -82,138 +80,10 @@ common::DataType GetHVDType(::xla::PrimitiveType type) {
   }
 }
 
-#if HAVE_GPU
-class XLAReadyEvent : public common::ReadyEvent {
-public:
-  XLAReadyEvent(cudaStream_t stream) : stream_(stream) {
-    CUDA_CALL(cudaEventCreate(&event_));
-    CUDA_CALL(cudaEventRecord(event_, stream));
-  }
-  ~XLAReadyEvent() { CUDA_CALL(cudaEventDestroy(event_)); }
-
-  bool Ready() const override {
-    cudaError_t result = cudaEventQuery(event_);
-    return cudaErrorNotReady != result;
-  }
-  gpuEvent_t event() const override { return event_; }
-
-private:
-  cudaStream_t stream_; // Not Owned.
-  cudaEvent_t event_;   // Owned.
-};
-#endif
-
-class XLATensor : public common::Tensor {
-public:
-  XLATensor(common::DataType type, common::TensorShape shape, void* buffer)
-      : type_(type), shape_(std::move(shape)), buffer_(buffer) {}
-
-  virtual const common::DataType dtype() const override { return type_; }
-  virtual const common::TensorShape shape() const override { return shape_; }
-  virtual const void* data() const override { return buffer_; }
-  virtual int64_t size() const override {
-    return shape_.num_elements() * common::DataType_Size(type_);
-  }
-
-protected:
-  common::DataType type_;
-  common::TensorShape shape_;
-  void* buffer_; // Not owned.
-};
-
-class XLAOpContext : public common::OpContext {
-public:
-  XLAOpContext(int device) : device_(device) {}
-
-  virtual common::Status AllocatePersistent(
-      int64_t size, std::shared_ptr<common::PersistentBuffer>* tensor) override;
-  virtual common::Status
-  AllocateOutput(common::TensorShape shape,
-                 std::shared_ptr<common::Tensor>* tensor) override;
-  // virtual common::Status
-  // AllocateOutput(int output_index, common::TensorShape shape,
-  //               std::shared_ptr<common::Tensor>* tensor) override;
-  virtual common::Status
-  AllocateZeros(int64_t num_elements, common::DataType dtype,
-                std::shared_ptr<common::Tensor>* tensor) override;
-  virtual common::Framework framework() const override;
-
-private:
-  int device_;
-};
-
-class XLAPersistentBuffer : public common::PersistentBuffer {
-public:
-  XLAPersistentBuffer(int device, int64_t size);
-  virtual const void*
-  AccessData(std::shared_ptr<common::OpContext> context) const override;
-
-private:
-  int device_;
-  void* buffer_;
-};
-
-XLAPersistentBuffer::XLAPersistentBuffer(int device, int64_t size)
-    : device_(device) {
-#if HAVE_CUDA
-  int restore_device;
-  CUDA_CALL(cudaGetDevice(&restore_device));
-  CUDA_CALL(cudaSetDevice(device));
-  CUDA_CALL(cudaMalloc((void**)&buffer_, size));
-  CUDA_CALL(cudaSetDevice(restore_device));
-#else
-  throw std::logic_error("Internal error. Requested XLAPersistentBuffer "
-                         "with GPU device but not compiled with CUDA.");
-#endif
-}
-
-const void* XLAPersistentBuffer::AccessData(
-    std::shared_ptr<common::OpContext> /*context*/) const {
-  return buffer_;
-}
-
-common::Status XLAOpContext::AllocatePersistent(
-    int64_t size, std::shared_ptr<common::PersistentBuffer>* tensor) {
-  *tensor = std::make_shared<XLAPersistentBuffer>(device_, size);
-  return common::Status::OK();
-}
-
-common::Status
-XLAOpContext::AllocateOutput(common::TensorShape shape,
-                             std::shared_ptr<common::Tensor>* tensor) {
-  // Let XLA allocate I/O buffers.
-  return common::Status::PreconditionError(
-      "AllocateOutput is not supported for XLA.");
-}
-
-// common::Status
-// XLAOpContext::AllocateOutput(int output_index, common::TensorShape shape,
-//                             std::shared_ptr<common::Tensor>* tensor) {
-//  // Let XLA allocate I/O buffers.
-//  return common::Status::PreconditionError(
-//      "AllocateOutput is not supported for XLA.");
-//}
-
-common::Status
-XLAOpContext::AllocateZeros(int64_t num_elements, common::DataType dtype,
-                            std::shared_ptr<common::Tensor>* tensor) {
-  // Let XLA allocate I/O buffers.
-  return common::Status::PreconditionError(
-      "AllocateZeros is not supported for XLA.");
-}
-
-common::Framework XLAOpContext::framework() const {
-  return common::Framework::XLA;
-}
-
-// On GPU this event will signal that data is ready, and tensors are
-// allocated.
-#if HAVE_GPU
-common::ReadyEvent* RecordReadyEvent(cudaStream_t stream) {
-  return new XLAReadyEvent(stream);
-}
-#endif
-
+// CustomCallConfig stores configurations of Horovod ops. We pass this config
+// to ::xla::CustomCall so that the XLA CustomCall can represent various Horovod
+// ops. Flatbuffer is used to serialize the config into string to conform to the
+// XLA CustomCall interface.
 class CustomCallConfig {
 public:
   std::string SerializeToString();
@@ -286,6 +156,12 @@ void CustomCallConfig::ParseFromString(std::string input) {
   }
 }
 
+// HVDAllreduceOp is an XLAOpKernel that lowers the Tensorflow HorovodAllreduce
+// op into XLA HLOs. The overall idea is to lower an Tensorflow op into HLO
+// `custom-calls`. See Compile() for detailed lowering. Custom callbacks can be
+// registered to XLA to implement different behaviors for custom-calls with
+// different names. See `CallbackHVDAllreduce` and `CallbackHVDAllreduceDone`
+// for exmaples.
 class HVDAllreduceOp : public XlaOpKernel {
 public:
   explicit HVDAllreduceOp(OpKernelConstruction* ctx) : XlaOpKernel(ctx) {
@@ -304,23 +180,23 @@ public:
       }
     }
 
-    // for DEBUG, use this to confirm the XLA lowering occurs in CI.
+    // for DEBUG ONLY, use this to confirm the XLA lowering occurs in CI.
     // TODO: remove it later.
     std::cerr << "XLA: Compile " << node_name_;
 
     // Generate below HLOs:
-    //     out = custom-call(in), custom_call_target="CallbackHVDAllreduce"
-    //     out' = custom-call(out),
+    //     start = custom-call(in), custom_call_target="CallbackHVDAllreduce"
+    //     end = custom-call(start),
     //         custom_call_target="CallbackHVDAllreduceDone"
-    // Note that tensors in, out, and out' are all aliased. It means that the
-    // all-reduce operation is in-place.
+    // Note that tensors `in`, `start`, and `end'` are aliased, as we want the
+    // all-reduce operation to be in-place.
     ::xla::XlaBuilder* const b = ctx->builder();
     // First, generate HVDAllreduce.
     std::vector<
         std::pair<::xla::ShapeIndex, std::pair<int64, ::xla::ShapeIndex>>>
         output_operand_aliasing = {
             {::xla::ShapeIndex{}, {0, ::xla::ShapeIndex{}}}};
-    auto input = ctx->Input(0);
+    ::xla::XlaOp input = ctx->Input(0);
     ::xla::XlaOp allreduce_start =
         b->ReportErrorOrReturn(BuildAllreduceCustomCall(
             b, {input}, /*is_start=*/true, output_operand_aliasing));
@@ -349,7 +225,7 @@ private:
   bool ignore_name_scope_;
 };
 
-// Implement this customized registrar so that we can make XLA Ops an opt-in,
+// Implements a customized registrar so that the registration is an opt-in,
 // controlled by HOROVOD_ENABLE_XLA_OPS.
 #define HVD_REGISTER_XLA_OP(NAME, OP)                                          \
   HVD_REGISTER_XLA_OP_UNIQ_HELPER(__COUNTER__, NAME, OP)
@@ -380,7 +256,7 @@ private:
 
 HVD_REGISTER_XLA_OP("HorovodAllreduce", HVDAllreduceOp);
 
-// Builds a custom call to a method named 'allreduce'.
+// A helper function to build HLOs for all-reduce.
 ::xla::StatusOr<::xla::XlaOp> HVDAllreduceOp::BuildAllreduceCustomCall(
     ::xla::XlaBuilder* b, absl::Span<const ::xla::XlaOp> operands,
     bool is_start,
@@ -407,6 +283,8 @@ HVD_REGISTER_XLA_OP("HorovodAllreduce", HVDAllreduceOp);
   return ::xla::CustomCall(
       b, call_target_name, operands, output_shape, config.SerializeToString(),
       /*has_side_effect=*/false, output_operand_aliasing, /*literal=*/nullptr,
+      // Special schedule hints are given so that XLA knows how to schedule
+      // the opague custom-calls for performance.
       is_start ? ::xla::CustomCallSchedule::EARLIEST
                : ::xla::CustomCallSchedule::LATEST);
 }
@@ -417,36 +295,42 @@ uint64 GetRendezvousKeyHash(const string& key) {
   return Hash64(k.data(), k.size());
 }
 
+// Implements a rendezvous to coordinate the `start` and `end` HLO callbacks.
+// An TF operation is broken into two HLOs to asynchronously interact with the
+// Horovod runtime. The `start` call is non-blocking and only the end call could
+// be blocking.
 class HVDCustomCallRendezvous {
 public:
   struct Payload {
     std::shared_ptr<gpuEvent_t> event;
   };
 
-  // This `Signal` method either a) flips the `Ready` value in the front of the
-  // queue if the front value is false, or otherwise b) enqueues a `Ready=true`
-  // into the queue.
+  // This `Signal` method places payload to be consumed by Wait().
+  //
   // Requirement: tensor_name shall be unique in a graph.
   void Signal(string tensor_name, common::Event hvd_event) {
+    // Use `tensor_name` to generate a hash value to retrieve the queue.
     uint64 key_hash = GetRendezvousKeyHash(tensor_name);
     mutex_lock l(mu_);
     InitQueue(key_hash);
 
     Queue& queue = *table_[key_hash];
     if (queue.empty() || queue.front() != nullptr) {
-      // No earlier waiters exist, so just leave a signal.
+      // No earlier waiters are waiting, so simply push a payload in the back.
       queue.push_back(new Payload{hvd_event.event});
       return;
     }
 
     // There is an earlier waiter to consume this signal. Place payload
-    // there.
+    // at the front of the queue where the waiter is polling.
+    CHECK(nullptr == queue.front());
     queue.front() = new Payload{hvd_event.event};
   }
 
-  // The `Wait` method waits for a `Ready==true` event. We assume there is at
-  // most one outstanding `Wait` call to simplfy the implementation. As such,
-  // this method always operates on the front value of the queue.
+  // The `Wait` method consumes Payloads. We assume there is at most one
+  // outstanding `Wait` call due to its blocking nature to simplify the
+  // implementation. Consequently, this method always operates on the very
+  // first item of the queue.
   void Wait(string tensor_name, CUstream stream) {
     uint64 key_hash = GetRendezvousKeyHash(tensor_name);
 
@@ -455,8 +339,8 @@ public:
       InitQueue(key_hash);
       Queue& queue = *table_[key_hash];
       if (queue.empty()) {
-        // So long as the queue is empty, place a signal but set it to false.
-        // We'll then wait for the `Signal` method to flip the Boolean value.
+        // So long as the queue is empty, place a NULL payload. Then waiting for
+        // Signal() to place the payload below.
         queue.push_back(nullptr);
       }
     }
@@ -469,23 +353,21 @@ public:
     while (!test_signal_value()) {
       // Busy waiting. As we don't anticipate the blocking occurs frequently,
       // this busy waiting should be fine. If this creates any performance
-      // overhead, implement conditional var wait.
+      // overhead, we may implement conditional var wait.
       std::this_thread::sleep_for(std::chrono::nanoseconds(100));
     }
 
     mutex_lock l(mu_);
     Queue* queue = table_[key_hash];
     Payload* payload = queue->front();
-    auto event = payload->event;
+    std::shared_ptr<gpuEvent_t> event = payload->event;
     queue->pop_front();
     if (queue->empty()) {
       table_.erase(key_hash);
       delete queue;
     }
     if (event) {
-#if HAVE_GPU
       CUDA_CALL(cudaStreamWaitEvent(stream, *event, /*flags=*/0));
-#endif
     }
     delete payload;
   }
@@ -502,7 +384,7 @@ private:
 private:
   // `nullptr` denotes non-readiness of the payload.
   typedef std::deque<Payload*> Queue;
-  // hash to queue.
+  // maps a hash value to queue.
   typedef absl::flat_hash_map<uint64, Queue*> Table;
 
   mutex mu_;
@@ -514,17 +396,132 @@ private:
   return self;
 }
 
-int GetDeviceOrdinal(void* ptr) {
-#if HAVE_GPU
-  cudaPointerAttributes attrs;
-  CUDA_CALL(cudaPointerGetAttributes(&attrs, ptr));
-  return attrs.device;
+class XLAReadyEvent : public common::ReadyEvent {
+public:
+  XLAReadyEvent(cudaStream_t stream) : stream_(stream) {
+    CUDA_CALL(cudaEventCreate(&event_));
+    CUDA_CALL(cudaEventRecord(event_, stream));
+  }
+  ~XLAReadyEvent() { CUDA_CALL(cudaEventDestroy(event_)); }
+
+  bool Ready() const override {
+    cudaError_t result = cudaEventQuery(event_);
+    return cudaErrorNotReady != result;
+  }
+  gpuEvent_t event() const override { return event_; }
+
+private:
+  cudaStream_t stream_; // Not Owned.
+  cudaEvent_t event_;   // Owned.
+};
+
+class XLATensor : public common::Tensor {
+public:
+  XLATensor(common::DataType type, common::TensorShape shape, void* buffer)
+      : type_(type), shape_(std::move(shape)), buffer_(buffer) {}
+
+  virtual const common::DataType dtype() const override { return type_; }
+  virtual const common::TensorShape shape() const override { return shape_; }
+  virtual const void* data() const override { return buffer_; }
+  virtual int64_t size() const override {
+    return shape_.num_elements() * common::DataType_Size(type_);
+  }
+
+protected:
+  common::DataType type_;
+  common::TensorShape shape_;
+  void* buffer_; // Not owned.
+};
+
+class XLAOpContext : public common::OpContext {
+public:
+  XLAOpContext(int device) : device_(device) {}
+
+  virtual common::Status AllocatePersistent(
+      int64_t size, std::shared_ptr<common::PersistentBuffer>* tensor) override;
+
+  virtual common::Status
+  AllocateOutput(common::TensorShape shape,
+                 std::shared_ptr<common::Tensor>* tensor) override;
+
+  virtual common::Status
+  AllocateZeros(int64_t num_elements, common::DataType dtype,
+                std::shared_ptr<common::Tensor>* tensor) override;
+
+  virtual common::Framework framework() const override {
+    return common::Framework::XLA;
+  }
+
+private:
+  int device_;
+};
+
+class XLAPersistentBuffer : public common::PersistentBuffer {
+public:
+  XLAPersistentBuffer(int device, int64_t size);
+  virtual const void*
+  AccessData(std::shared_ptr<common::OpContext> context) const override;
+
+private:
+  int device_;
+  void* buffer_;
+};
+
+XLAPersistentBuffer::XLAPersistentBuffer(int device, int64_t size)
+    : device_(device) {
+#if HAVE_CUDA
+  int restore_device;
+  CUDA_CALL(cudaGetDevice(&restore_device));
+  CUDA_CALL(cudaSetDevice(device));
+  // Simply call cudaMalloc for persistent buffer.
+  CUDA_CALL(cudaMalloc((void**)&buffer_, size));
+  CUDA_CALL(cudaSetDevice(restore_device));
 #else
-  return CPU_DEVICE_ID;
+  throw std::logic_error("Internal error. Requested XLAPersistentBuffer "
+                         "with GPU device but not compiled with CUDA.");
 #endif
 }
 
-// Custom calls
+const void* XLAPersistentBuffer::AccessData(
+    std::shared_ptr<common::OpContext> /*context*/) const {
+  return buffer_;
+}
+
+common::Status XLAOpContext::AllocatePersistent(
+    int64_t size, std::shared_ptr<common::PersistentBuffer>* tensor) {
+  *tensor = std::make_shared<XLAPersistentBuffer>(device_, size);
+  return common::Status::OK();
+}
+
+common::Status
+XLAOpContext::AllocateOutput(common::TensorShape shape,
+                             std::shared_ptr<common::Tensor>* tensor) {
+  // XLA must manage I/O buffers.
+  return common::Status::PreconditionError(
+      "AllocateOutput is not supported for XLA.");
+}
+
+common::Status
+XLAOpContext::AllocateZeros(int64_t num_elements, common::DataType dtype,
+                            std::shared_ptr<common::Tensor>* tensor) {
+  // XLA must manage I/O buffers.
+  return common::Status::PreconditionError(
+      "AllocateZeros is not supported for XLA.");
+}
+
+// On GPU this event will signal that data is ready, and tensors are
+// allocated.
+common::ReadyEvent* RecordReadyEvent(cudaStream_t stream) {
+  return new XLAReadyEvent(stream);
+}
+
+int GetDeviceOrdinal(void* ptr) {
+  cudaPointerAttributes attrs;
+  CUDA_CALL(cudaPointerGetAttributes(&attrs, ptr));
+  return attrs.device;
+}
+
+// Implements the `HVDAllreduce` HLO CustomCall.
 void CallbackHVDAllreduce(CUstream stream, void** buffers, const char* opaque,
                           size_t opaque_len) {
   CustomCallConfig config;
@@ -532,10 +529,8 @@ void CallbackHVDAllreduce(CUstream stream, void** buffers, const char* opaque,
 
   // Enqueue requests.
   common::ReadyEventList ready_event_list;
-#if HAVE_GPU
   ready_event_list.AddReadyEvent(
       std::shared_ptr<common::ReadyEvent>(RecordReadyEvent(stream)));
-#endif
   int dev_ordinal = GetDeviceOrdinal(buffers[0]);
   auto hvd_context = std::make_shared<XLAOpContext>(dev_ordinal);
   auto hvd_input = std::make_shared<XLATensor>(
@@ -544,7 +539,7 @@ void CallbackHVDAllreduce(CUstream stream, void** buffers, const char* opaque,
   auto hvd_output = std::make_shared<XLATensor>(
       config.tensor_type_, common::TensorShape(config.input_shapes_[0]),
       buffers[1]);
-  auto enqueue_result = EnqueueTensorAllreduce(
+  common::Status enqueue_result = EnqueueTensorAllreduce(
       hvd_context, hvd_input, hvd_output, ready_event_list, config.tensor_name_,
       dev_ordinal,
       [=](const common::Status& status) {
@@ -556,6 +551,7 @@ void CallbackHVDAllreduce(CUstream stream, void** buffers, const char* opaque,
   CHECK(enqueue_result.ok()) << enqueue_result.reason();
 }
 
+// Implements the `HVDAllreduceDone` HLO CustomCall.
 void CallbackHVDAllreduceDone(CUstream stream, void** /*buffers*/,
                               const char* opaque, size_t opaque_len) {
   // Blocking until the request is done processing.
@@ -572,3 +568,5 @@ XLA_REGISTER_CUSTOM_CALL_TARGET(CallbackHVDAllreduceDone, "CUDA");
 } // namespace
 } // namespace tensorflow
 } // namespace horovod
+
+#endif // HAVE_GPU
