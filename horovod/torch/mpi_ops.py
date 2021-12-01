Fix PyTorch gradient for reducescatter and add process set tests
Signed-off-by: Max H. Gerlach <git@maxgerlach.de>

horovod/torch/mpi_ops.py
@@ -968,7 +968,7 @@ class HorovodReducescatter(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_output):
         if ctx.op == Sum:
-            grad_output *= size()
+            grad_output *= ctx.process_set.size()
 
         return allgather(grad_output, process_set=ctx.process_set), None, None, None
 

test/parallel/test_torch.py
@@ -3250,7 +3250,7 @@ class TorchTests(unittest.TestCase):
         self.assertTrue(barrier_time >= 5)
 
     def test_horovod_reducescatter(self):
-        """Test that the allreduce correctly sums and scatters 1D, 2D, 3D tensors."""
+        """Test that reducescatter correctly sums and scatters 1D, 2D, 3D tensors."""
         if not hvd.mpi_built():
             self.skipTest("Reducescatter is not yet implemented in gloo/mlsl")
 
@@ -3291,7 +3291,7 @@ class TorchTests(unittest.TestCase):
 
 
     def test_horovod_reducescatter_average(self):
-        """Test that the allreduce correctly averages and scatters 1D, 2D, 3D tensors."""
+        """Test that reducescatter correctly averages and scatters 1D, 2D, 3D tensors."""
         if not hvd.mpi_built():
             self.skipTest("Reducescatter is not yet implemented in gloo/mlsl")
 
@@ -3559,6 +3559,123 @@ class TorchTests(unittest.TestCase):
                             "gradient %s differs from expected %s, "
                             "error: %s" % (grad_out, expected, str(err)))
 
+
+    def test_horovod_reducescatter_process_sets(self):
+        """Test that reducescatter correctly sums and scatters 1D, 2D, 3D tensors if restricted 
+        to non-global process sets."""
+        if not hvd.mpi_built():
+            self.skipTest("Reducescatter is not yet implemented in gloo/mlsl")
+        if hvd.ccl_built():
+            self.skipTest("Multiple process sets currently do not support CCL.")
+
+        hvd.init()
+        rank = hvd.rank()
+        size = hvd.size()
+        
+        even_ranks = [rk for rk in range(0, size) if rk % 2 == 0]
+        odd_ranks = [rk for rk in range(0, size) if rk % 2 == 1]
+
+        even_set = hvd.add_process_set(even_ranks)
+        odd_set = hvd.add_process_set(odd_ranks)
+        
+        if rank in even_ranks:
+            this_set = even_set
+        if rank in odd_ranks:
+            this_set = odd_set
+        
+        dtypes = self.filter_supported_types([torch.IntTensor, torch.LongTensor,
+                                              torch.FloatTensor, torch.DoubleTensor,
+                                              torch.HalfTensor])
+        if torch.cuda.is_available():
+            dtypes += [torch.cuda.IntTensor, torch.cuda.LongTensor,
+                       torch.cuda.FloatTensor, torch.cuda.DoubleTensor,
+                       torch.cuda.HalfTensor]
+        dims = [1, 2, 3]
+        for dtype, dim in itertools.product(dtypes, dims):
+            torch.manual_seed(1234)
+            even_rank_tensor = torch.FloatTensor(*([len(even_ranks) * 4] * dim)).random_(-100, 100)
+            odd_rank_tensor = torch.FloatTensor(*([len(odd_ranks) * 4] * dim)).random_(-100, 100)
+            if rank in even_ranks:
+                tensor = self.cast_and_place(even_rank_tensor, dtype)
+                summed = hvd.reducescatter(tensor, op=hvd.Sum, process_set=even_set)
+            elif rank in odd_ranks:
+                tensor = self.cast_and_place(odd_rank_tensor, dtype)
+                summed = hvd.reducescatter(tensor, op=hvd.Sum, process_set=odd_set)
+            tensor, summed = self.convert_cpu_fp16_to_fp32(tensor, summed)
+            expected = tensor[this_set.rank() * 4:(this_set.rank() + 1) * 4] * this_set.size()
+
+            # Threshold for floating point equality depends on number of
+            # ranks, since we're comparing against precise multiplication.
+            if this_set.size() <= 3 or dtype in [torch.IntTensor, torch.LongTensor,
+                                                 torch.cuda.IntTensor, torch.cuda.LongTensor]:
+                threshold = 0
+            elif this_set.size() < 10:
+                threshold = 1e-4
+            elif this_set.size() < 15:
+                threshold = 5e-4
+            else:
+                break
+
+            assert list(summed.shape) == list(expected.shape)
+            max_difference = summed.data.sub(expected).max()
+            assert max_difference <= threshold, 'hvd.reducescatter produces incorrect results'
+
+        hvd.remove_process_set(odd_set)
+        hvd.remove_process_set(even_set)
+
+
+    def test_horovod_reducescatter_grad_process_sets(self):
+        """Test the correctness of the reducescatter gradient if restricted to non-global process sets."""
+        if not hvd.mpi_built():
+            return  # Reducescatter is not yet implemented in gloo/mlsl
+        if hvd.ccl_built():
+            self.skipTest("Multiple process sets currently do not support CCL.")
+
+        hvd.init()
+        size = hvd.size()
+        rank = hvd.rank()
+
+        even_ranks = [rk for rk in range(0, size) if rk % 2 == 0]
+        odd_ranks = [rk for rk in range(0, size) if rk % 2 == 1]
+
+        even_set = hvd.add_process_set(even_ranks)
+        odd_set = hvd.add_process_set(odd_ranks)
+        if rank in even_ranks:
+            this_set = even_set
+        if rank in odd_ranks:
+            this_set = odd_set
+
+
+        # Only Tensors of floating point dtype can require gradients
+        dtypes = [torch.FloatTensor, torch.DoubleTensor, torch.HalfTensor]
+        if torch.cuda.is_available():
+            dtypes += [torch.cuda.FloatTensor, torch.cuda.DoubleTensor, torch.cuda.HalfTensor]
+        dims = [1, 2, 3]
+        for dtype, dim in itertools.product(dtypes, dims):
+            torch.manual_seed(1234)
+            even_rank_tensor = torch.FloatTensor(*([even_set.size() * 4] * dim)).random_(-100, 100)
+            odd_rank_tensor = torch.FloatTensor(*([odd_set.size() * 4] * dim)).random_(-100, 100)
+            if rank in even_ranks:
+                tensor = self.cast_and_place(even_rank_tensor, dtype)
+                this_set = even_set
+            elif rank in odd_ranks:
+                tensor = self.cast_and_place(odd_rank_tensor, dtype)
+                this_set = odd_set
+            tensor.requires_grad_()
+            summed = hvd.reducescatter(tensor, op=hvd.Sum, process_set=this_set)
+
+            grad_shape = [4] + [this_set.size() * 4] * (dim - 1)
+            summed.backward(self.cast_and_place(torch.ones(grad_shape), dtype))
+            grad_out = tensor.grad.data.cpu().numpy()
+
+            expected = np.ones([this_set.size() * 4] * dim) * this_set.size()
+            err = np.linalg.norm(expected - grad_out)
+            self.assertLess(err, 0.00000001,
+                            "gradient %s differs from expected %s, "
+                            "error: %s" % (grad_out, expected, str(err)))
+
+        hvd.remove_process_set(odd_set)
+        hvd.remove_process_set(even_set)
 
 
 if __name__ == "__main__":