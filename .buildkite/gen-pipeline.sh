6a74e341c42fe89eb1a8995252ebe6a68fa4cc64
Enrico Minack <github@enrico.minack.dev>
2021-12-27 10:09:24 +0100

    Move torch to patch release 1.10.1
    Signed-off-by: Enrico Minack <github@enrico.minack.dev>

--- .buildkite/gen-pipeline.sh
+++ .buildkite/gen-pipeline.sh
@@ -7,7 +7,7 @@ set -eu
 repository=823773083436.dkr.ecr.us-east-1.amazonaws.com/buildkite
 
 # our baseline test is
-baseline="test-cpu-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_0-mxnet1_9_0-pyspark3_2_0"
+baseline="test-cpu-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_1-mxnet1_9_0-pyspark3_2_0"
 # in run_gloo_integration we run 'Elastic Spark * Tests' for this baseline
 # so it has to have Gloo mpi kind
 
@@ -17,19 +17,19 @@ code_files=$(python "$dir/get_changed_code_files.py" || echo failure)
 tests=$(if [[ -n "${PIPELINE_MODE:-}" ]] && ( [[ "${BUILDKITE_BRANCH:-}" == "${BUILDKITE_PIPELINE_DEFAULT_BRANCH:-}" ]] || [[ -n "$code_files" ]] ); then
   # we vary the baseline along the Python dimension and PySpark together
   # run_gloo_integration expects these to have Gloo mpi kind to run 'Elastic Spark * Tests'
-  printf "test-cpu-gloo-py3_7-tf2_7_0-keras2_7_0-torch1_10_0-mxnet1_9_0-pyspark2_4_8 "
-  printf "test-cpu-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_0-mxnet1_9_0-pyspark3_1_2 "
+  printf "test-cpu-gloo-py3_7-tf2_7_0-keras2_7_0-torch1_10_1-mxnet1_9_0-pyspark2_4_8 "
+  printf "test-cpu-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_1-mxnet1_9_0-pyspark3_1_2 "
   # our baseline
   printf "$baseline "
 
   # then we vary the baseline along mpi kinds dimension
   # our baseline again
-# printf "test-cpu-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_0-mxnet1_9_0-pyspark3_2_0 "
-  printf "test-cpu-mpich-py3_8-tf2_7_0-keras2_7_0-torch1_10_0-mxnet1_9_0-pyspark3_2_0 "
-  printf "test-cpu-oneccl-py3_8-tf2_7_0-keras2_7_0-torch1_10_0-mxnet1_9_0-pyspark3_2_0 "
-  printf "test-cpu-openmpi-py3_8-tf2_7_0-keras2_7_0-torch1_10_0-mxnet1_9_0-pyspark3_2_0 "
+# printf "test-cpu-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_1-mxnet1_9_0-pyspark3_2_0 "
+  printf "test-cpu-mpich-py3_8-tf2_7_0-keras2_7_0-torch1_10_1-mxnet1_9_0-pyspark3_2_0 "
+  printf "test-cpu-oneccl-py3_8-tf2_7_0-keras2_7_0-torch1_10_1-mxnet1_9_0-pyspark3_2_0 "
+  printf "test-cpu-openmpi-py3_8-tf2_7_0-keras2_7_0-torch1_10_1-mxnet1_9_0-pyspark3_2_0 "
   # note: we test openmpi-gloo mpi kind in this variation in each of [cpu, gpu, mixed]
-  printf "test-cpu-openmpi-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_0-mxnet1_9_0-pyspark3_2_0 "
+  printf "test-cpu-openmpi-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_1-mxnet1_9_0-pyspark3_2_0 "
 
   # then we vary the baseline along the framework dimensions all together
   # some frameworks are not available for our baseline Python version 3.8, so we use Python 3.7
@@ -41,7 +41,7 @@ tests=$(if [[ -n "${PIPELINE_MODE:-}" ]] && ( [[ "${BUILDKITE_BRANCH:-}" == "${B
   printf "test-cpu-gloo-py3_8-tf2_5_1-keras2_5_0rc0-torch1_8_1-mxnet1_7_0_p2-pyspark3_2_0 "
   printf "test-cpu-gloo-py3_8-tf2_6_0-keras2_6_0-torch1_9_1-mxnet1_8_0_p0-pyspark3_2_0 "
   # our baseline again
-# printf "test-cpu-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_0-mxnet1_9_0-pyspark3_2_0 "
+# printf "test-cpu-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_1-mxnet1_9_0-pyspark3_2_0 "
   printf "test-cpu-gloo-py3_8-tfhead-keras_none-torchhead-mxnethead-pyspark3_2_0 "
 
   # then we vary the frameworks for gpu
@@ -52,11 +52,11 @@ tests=$(if [[ -n "${PIPELINE_MODE:-}" ]] && ( [[ "${BUILDKITE_BRANCH:-}" == "${B
   # here we deviate from mxnet==1.7.0.post2 as there is none for cu101, only post1
   printf "test-gpu-gloo-py3_8-tf2_5_1-keras2_5_0rc0-torch1_8_1-mxnet1_7_0_p1-pyspark3_2_0 "
   printf "test-gpu-gloo-py3_8-tf2_6_0-keras2_6_0-torch1_9_1-mxnet1_8_0_p0-pyspark3_2_0 "
-  printf "test-gpu-openmpi-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_0-mxnet1_9_0-pyspark3_2_0 "
+  printf "test-gpu-openmpi-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_1-mxnet1_9_0-pyspark3_2_0 "
   printf "test-gpu-gloo-py3_8-tfhead-keras_none-torchhead-mxnethead-pyspark3_2_0 "
 
   # and one final test with mixed cpu+gpu
-  printf "test-mixed-openmpi-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_0-mxnet1_9_0-pyspark3_2_0 "
+  printf "test-mixed-openmpi-gloo-py3_8-tf2_7_0-keras2_7_0-torch1_10_1-mxnet1_9_0-pyspark3_2_0 "
 fi | if [[ "${PIPELINE_MODE:-}" == "GPU"* ]]; then sed -E "s/[^ ]*-cpu-[^ ]*//g"; else cat; fi \
    | if [[ "${PIPELINE_MODE:-}" == "GPU HEADS" ]]; then sed -E "s/ /\n/g" | grep -e "-tfhead-keras_none-torchhead-mxnethead-" | paste -s -d " " -; else cat; fi \
    | if [[ "${PIPELINE_MODE:-}" == "GPU NON HEADS" ]]; then sed -E "s/[^ ]*-tfhead-keras_none-torchhead-mxnethead-[^ ]*//g"; else cat; fi)
