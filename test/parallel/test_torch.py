abea4a0f05e52c6360e16695ca0ef7276d86bdca
Max H. Gerlach <git@maxgerlach.de>
2022-01-07 18:00:37 +0000

    TorchTests::test_broadcast_state: Add explicit names to broadcast operations
    I am not sure, but hangs might have been caused by wrongly associated
    autogenerated names like `broadcast.noname.1114`.
    
    Signed-off-by: Max H. Gerlach <git@maxgerlach.de>

--- test/parallel/test_torch.py
+++ test/parallel/test_torch.py
@@ -2159,14 +2159,14 @@ class TorchTests(unittest.TestCase):
 
             model_param_values = get_model_param_values(model)
             for name, model_param_value in model_param_values:
-                hvd.broadcast_(model_param_value, root_rank=0)
+                hvd.broadcast_(model_param_value, root_rank=0, name=name)
 
             opt_param_values_updated = []
             opt_param_values = get_optimizer_param_values(optimizer)
             for name, opt_param_value in opt_param_values:
                 is_tensor = torch.is_tensor(opt_param_value)
                 if is_tensor:
-                    hvd.broadcast_(opt_param_value, root_rank=0)
+                    hvd.broadcast_(opt_param_value, root_rank=0, name=f"{name}_tensor")
                 else:
                     opt_param_value = hvd.broadcast_object(opt_param_value, name=name)
                 opt_param_values_updated.append((name, opt_param_value))
