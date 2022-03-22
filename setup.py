58bc4bb996835b95ee985d7a6491e93a17c7444c
Enrico Minack <github@enrico.minack.dev>
2022-03-22 11:50:56 +0100

    Relax pytorch_lightning to '>=1.3.8,<1.5.10'
    Signed-off-by: Enrico Minack <github@enrico.minack.dev>

--- setup.py
+++ setup.py
@@ -162,7 +162,7 @@ tensorflow_cpu_require_list = ['tensorflow-cpu']
 tensorflow_gpu_require_list = ['tensorflow-gpu']
 keras_require_list = ['keras>=2.0.8,!=2.0.9,!=2.1.0,!=2.1.1']
 # pytorch-lightning 1.3.8 is a stable version to work with horovod
-pytorch_require_list = ['torch', 'pytorch_lightning==1.3.8']
+pytorch_require_list = ['torch', 'pytorch_lightning>=1.3.8,<1.5.10']
 mxnet_require_list = ['mxnet>=1.4.1']
 pyspark_require_list = ['pyspark>=2.3.2;python_version<"3.8"',
                         'pyspark>=3.0.0;python_version>="3.8"']
@@ -189,7 +189,7 @@ dev_require_list = ['tensorflow-cpu==2.2.0',
                     'keras==2.3.1',
                     'torch==1.4.0',
                     'torchvision==0.5.0',
-                    'pytorch_lightning>=1.3.8',
+                    'pytorch_lightning>=1.3.8,<1.5.10',
                     'mxnet==1.5.0',
                     'pyspark==3.0.1'] + spark_require_list
 # torchvision 0.5.0 depends on torch==1.4.0
