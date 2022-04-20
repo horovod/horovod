a526990baeac7e0711c6486640d27cdf62d15ffa
Enrico Minack <github@enrico.minack.dev>
2022-04-20 21:34:00 +0100

    Make spark worker work with spark-submit
    Signed-off-by: Enrico Minack <github@enrico.minack.dev>

--- horovod/spark/tensorflow/compute_worker.py
+++ horovod/spark/tensorflow/compute_worker.py
@@ -1,60 +1,66 @@
+# Copyright 2022 G-Research. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
 import argparse
 import logging
-import os
 import signal
 import sys
-from datetime import datetime
 
-from horovod.runner.common.util import secret
-from horovod.spark import run
-from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
-from nereus.tf_data_service import TfDataServiceConfig, compute_worker_fn
-from nereus.tf_data_service.compute_service import ComputeService
-from nereus.utils.logging_utils import initialise_default_logging, Tee
-
-initialise_default_logging(os.path.basename(__file__))
-
+from horovod.runner.common.service.compute_service import ComputeService
+from horovod.runner.common.util import secret
+from horovod.spark import run
+from horovod.tensorflow.data.compute_service import TfDataServiceConfig, compute_worker_fn
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--compute-service-config-file", required=True, type=str,
-                        help=f"The path to store the compute service config file.",
-                        dest="compute_service_config_file")
+    parser.add_argument("configfile", type=str,
+                        help=f"The path to store the compute service config file.")
 
-    parser.add_argument("--dispatchers", required=True, type=int,
+    parser.add_argument("--dispatchers", required=False, default=1, type=int,
                         help=f"The number of dispatcher to support.",
                         dest="dispatchers")
 
-    parser.add_argument("--workers-per-dispatcher", required=True, type=int,
-                        help=f"The number of workers per dispatcher.",
-                        dest="workers_per_dispatcher")
-
-    parser.add_argument("--dispatcher-side", required=True, type=str,
+    parser.add_argument("--dispatcher-side", required=False, default='compute', type=str,
                         help=f"Where do the dispatcher run? On 'compute' side or 'training' side.",
                         dest="dispatcher_side")
 
     parsed_args = parser.parse_args()
-    workers = parsed_args.dispatchers * parsed_args.workers_per_dispatcher
+
+    spark = SparkSession.builder.getOrCreate()
+    spark_context = spark.sparkContext
+    workers = spark_context.defaultParallelism
+
+    if workers % parsed_args.dispatchers:
+        raise ValueError(f'Number of processes ({workers}) must be '
+                         f'a multiple of number of dispatchers ({parsed_args.dispatchers}).')
+    workers_per_dispatcher = workers // parsed_args.dispatchers
 
     key = secret.make_secret_key()
-    compute = ComputeService(parsed_args.dispatchers, parsed_args.workers_per_dispatcher, key=key)
+    compute = ComputeService(parsed_args.dispatchers, workers_per_dispatcher, key=key)
 
     compute_config = TfDataServiceConfig(
         dispatchers=parsed_args.dispatchers,
-        workers_per_dispatcher=parsed_args.workers_per_dispatcher,
+        workers_per_dispatcher=workers_per_dispatcher,
         dispatcher_side=parsed_args.dispatcher_side,
         addresses=compute.addresses(),
         key=key
     )
-    compute_config.write(parsed_args.compute_service_config_file)
-
-    conf = SparkConf()
-    conf.setMaster(f'local[{workers}]').setAppName('compute')
-    spark = SparkSession.builder.config(conf=conf).getOrCreate()
-    spark_context = spark.sparkContext
+    compute_config.write(parsed_args.configfile)
 
     def _exit_gracefully():
         logging.info('Spark driver receiving SIGTERM. Exiting gracefully')
@@ -64,8 +70,8 @@ if __name__ == '__main__':
 
     ret = run(compute_worker_fn,
               args=(compute_config,),
-              stdout=Tee(f'nereus-compute-logs/stdout/{datetime.now().strftime("%Y%m%d-%H%M%S")}.log', mode='w'),
-              stderr=Tee(f'nereus-compute-logs/stderr/{datetime.now().strftime("%Y%m%d-%H%M%S")}.log', mode='w'),
+              stdout=sys.stdout,
+              stderr=sys.stderr,
               num_proc=workers,
               verbose=2)
 
