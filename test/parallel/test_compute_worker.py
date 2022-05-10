d98faf3006191ae6340d4be002e467e94957746b
Enrico Minack <github@enrico.minack.dev>
2022-05-10 09:44:42 +0100

    Syncronize tests, assert batches
    Signed-off-by: Enrico Minack <github@enrico.minack.dev>

--- test/parallel/test_compute_worker.py
+++ test/parallel/test_compute_worker.py
@@ -16,10 +16,11 @@ import os
 import unittest
 from distutils.version import LooseVersion
 from itertools import islice
-from typing import List
+from typing import List, Callable
+
 import tensorflow as tf
 
-from horovod.runner.common.util import env
+import horovod.tensorflow as hvd
 from horovod.runner.util.threads import in_thread
 from horovod.tensorflow.data.compute_service import TfDataServiceConfig
 from horovod.tensorflow.data.compute_worker import main
@@ -33,37 +34,78 @@ class ComputeWorkerTest(unittest.TestCase):
     timeout = 10
 
     # rank and size of this test
-    rank, size = env.get_env_rank_and_size()
+    hvd.init()
+    rank, size = hvd.rank(), hvd.size()
+
+    @property
+    def expected_cluster_shape(self):
+        return [(r, self.size) for r in range(self.size)]
 
     def test_single_dispatcher(self):
-        self.do_test_worker(1, reuse_dataset=False, round_robin=False,
-                            expected_batches=[[[i for i in range(batch*128, (batch+1)*128)]
-                                               for batch in range(8)]] * self.size)
+        def assert_batches(actuals: List[List[List[int]]]):
+            # batches are not deterministic, all we can say is the shape of the batches
+            # and each number in [0..15] appears once
+            self.assertEqual(self.size, len(actuals))
+            for actual in actuals:
+                self.assertEqual(4, len(actual))
+                self.assertEqual([4, 4, 4, 4], [len(batch) for batch in actual])
+                self.assertEqual({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+                                 {i for batch in actual for i in batch})
 
-    def test_single_dispatcher_reuse_dataset(self):
-        self.do_test_worker(1, reuse_dataset=True, round_robin=False, expected_batches=[[[]]] * self.size)
+        self.do_test_worker(1, reuse_dataset=False, round_robin=False, assert_batches=assert_batches)
+
+    def test_single_dispatcher_reuse_dataset_fcfs(self):
+        def assert_batches(actuals: List[List[List[int]]]):
+            # batches are not deterministic, all we can say is all but the last batch have length 4,
+            # the last has at most 4, all numbers in [0..15] appear once across all workers
+            for actual in actuals:
+                if len(actual) > 0:
+                    self.assertEqual([4] * (len(actual) - 1), [len(batch) for batch in actual[:-1]])
+                    self.assertTrue(len(actual[-1]) <= 4)
+
+            actuals = [i for actual in actuals for batch in actual for i in batch]
+            self.assertEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], sorted(actuals))
+
+        self.do_test_worker(1, reuse_dataset=True, round_robin=False, assert_batches=assert_batches)
 
     def test_single_dispatcher_reuse_dataset_round_robin(self):
-        self.do_test_worker(1, reuse_dataset=True, round_robin=True, expected_batches=[[[]]] * self.size)
+        def assert_batches(actuals: List[List[List[int]]]):
+            # batches are not deterministic, all we can say is the shape of the batches
+            # and each number in [0..15] appears once across all workers
+            for actual in actuals:
+                self.assertEqual(2, len(actual))
+                self.assertEqual([4, 4], [len(batch) for batch in actual])
 
-    def test_two_dispatchers_compute_side(self):
-        self.do_test_worker(2, reuse_dataset=True, round_robin=True, expected_batches=[[[]]] * self.size)
+            actuals = [i for actual in actuals for batch in actual for i in batch]
+            self.assertEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], sorted(actuals))
+
+        self.do_test_worker(1, reuse_dataset=True, round_robin=True, assert_batches=assert_batches)
+
+    def test_two_dispatchers(self):
+        def assert_batches(actuals: List[List[List[int]]]):
+            for actual in actuals:
+                self.assertEqual([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15]], actual)
+
+        self.do_test_worker(2, reuse_dataset=False, round_robin=False, assert_batches=assert_batches)
 
     def do_test_worker(self,
                        dispatchers: int,
                        reuse_dataset: bool,
                        round_robin: bool,
-                       expected_batches: List[List[List[int]]]):
-        self.do_test_worker_compute_side(dispatchers, reuse_dataset=reuse_dataset, round_robin=round_robin,
-                                         expected_batches=expected_batches)
+                       assert_batches: Callable[[List[List[List[int]]]], None]):
+        self.do_test_worker_compute_side(dispatchers, reuse_dataset=reuse_dataset, round_robin=round_robin, assert_batches=assert_batches)
 
     def do_test_worker_compute_side(self,
                                     dispatchers: int,
                                     reuse_dataset: bool,
                                     round_robin: bool,
-                                    expected_batches: List[List[List[int]]]):
-        # check expected batches have right size
-        self.assertEqual(self.size, len(expected_batches), msg=f'expected batches must have length of {self.size}: {len(expected_batches)}')
+                                    assert_batches: Callable[[List[List[List[int]]]], None]):
+        # synchronize with all processes
+        print('waiting for all processes to get started')
+        self.assertTrue(self.size > 1)
+        cluster_shape = hvd.allgather_object((self.rank, self.size), name='test_start')
+        self.assertEqual(self.expected_cluster_shape, cluster_shape)
+        print('all processes started')
 
         # the config file for this worker
         configfile = __file__ + '.config'
@@ -79,21 +121,32 @@ class ComputeWorkerTest(unittest.TestCase):
 
             try:
                 # Allow tf.data service to pre-process the pipeline
-                dataset = tf.data.Dataset.range(1024)
+                dataset = tf.data.Dataset.range(16)
                 if round_robin:
                     dataset = dataset.repeat()
-                dataset = dataset.batch(128) \
+                dataset = dataset.batch(4) \
                     .send_to_data_service(compute_config, self.rank, self.size,
                                           reuse_dataset=reuse_dataset,
-                                          round_robin=round_robin) \
-                    .prefetch(4)
+                                          round_robin=round_robin)
+
+                # sync again before fetching dataset
+                hvd.allgather_object((self.rank, self.size), name='test_start')
 
                 # fetch the batches
                 it = dataset.as_numpy_iterator()
                 if round_robin:
-                    it = islice(it, 8)
+                    it = islice(it, 2)
 
-                self.assertEqual(expected_batches[self.rank], [batch.tolist() for batch in it])
+                actual = list([batch.tolist() for batch in it])
+
+                # synchronize with all processes
+                print('waiting for all processes to finish')
+                actuals = hvd.allgather_object(actual)
+                print('all processes finished')
+
+                # assert the provided batches
+                self.assertEqual(self.size, len(actuals))
+                assert_batches(actuals)
 
             finally:
                 # shutdown compute service
@@ -101,10 +154,11 @@ class ComputeWorkerTest(unittest.TestCase):
                     compute = compute_config.compute_client(verbose=2)
                     compute.shutdown()
 
-            # wait for the worker to terminate
-            worker.join(self.timeout)
-            self.assertFalse(worker.is_alive())
+                # wait for the worker to terminate
+                worker.join(self.timeout)
+                #self.assertFalse(worker.is_alive())
 
         finally:
+            # remove the configfile as it will interfere with subsequent runs of this test
             if self.rank == 0:
                 os.unlink(configfile)
