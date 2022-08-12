31dff680301c7d4676429282f961a9b1982cf501
Ata FatahiBaarzi <afatahibaarzi@linkedin.com>
2022-08-12 09:33:50 -0700

    use existing DistributedGradientTape API in partial tape
    Signed-off-by: Ata FatahiBaarzi <afatahibaarzi@linkedin.com>

--- horovod/tensorflow/__init__.py
+++ horovod/tensorflow/__init__.py
@@ -1031,7 +1031,7 @@ if hasattr(tf, 'GradientTape'):
                                 compression=Compression.none, sparse_as_dense=False,
                                 op=Average, gradient_predivide_factor=1.0,
                                 num_groups=0, groups=None, process_set=global_process_set,
-                                local_layers=[]):
+                                local_layers=None):
         """A tape that wraps another tf.GradientTape, using an allreduce to
         combine gradient values before applying gradients to model weights similar to
         DistributedGradientTape execpt it skips allreducing gradients of the local layers
@@ -1041,47 +1041,26 @@ if hasattr(tf, 'GradientTape'):
           gradtape:
             GradientTape to use for computing gradients and applying updates.
           local_layers:
-            A list of tf.keras.Layer local layers that their gradients need not
+            A collection of type tf.keras.layers.Layer local layers that their gradients need not
             to be synced accross ranks and is kept and applied locally.
             If not provided, the functionality of PartialDistributedGradientTape is
             identical to DistributedGradientTape.
 
         The rest of the arguments are similar to those of DistributedGradientTape.
         """
-        if gradient_predivide_factor != 1.0:
-            if rocm_built():
-                raise ValueError('gradient_predivide_factor not supported yet with ROCm')
-            if op != Average:
-                raise ValueError('gradient_predivide_factor not supported with op != Average')
-
-        if num_groups != 0:
-            warnings.warn('Parameter `num_groups` has been replaced by `groups` '
-                          'and will be removed in v0.23.0.', DeprecationWarning)
-            if groups is None:
-                groups = num_groups
-
-        if groups is not None:
-            if not (isinstance(groups, list) or groups > 0):
-                raise ValueError('groups should be a non-negative integer or '
-                                'a list of list of tf.Variable.')
-
-        if local_layers is not None:
-            if not isinstance(local_layers, list):
-                raise ValueError('local_layers should be a list of layers of type tf.keras.Layer.')
+        if local_layers:
+            if isinstance(local_layers, tf.keras.layers.Layer):
+                local_layers = [local_layers]
+            else:
+                if not all(isinstance(layer, tf.keras.layers.Layer) for layer in local_layers):
+                    raise ValueError("All local layers must be of tf.keras.Layer type.")
 
         local_vars = [var for layer in local_layers for var in layer.trainable_weights]
 
-        cls = type(gradtape.__class__.__name__, (gradtape.__class__,),
-                   dict(_DistributedGradientTape.__dict__))
-        if hasattr(gradtape, '_watch_accessed_variables'):
-            _tape = cls(gradtape._tape, device_dense, device_sparse, compression,
-                       sparse_as_dense, op, gradient_predivide_factor, groups,
-                       gradtape._persistent, gradtape._watch_accessed_variables,
-                       process_set=process_set)
-        else:
-            _tape = cls(gradtape._tape, device_dense, device_sparse, compression,
-                       sparse_as_dense, op, gradient_predivide_factor, groups,
-                       gradtape._persistent, process_set=process_set)
+        _tape = DistributedGradientTape(gradtape, device_dense, device_sparse,
+                                compression, sparse_as_dense,
+                                op, gradient_predivide_factor,
+                                num_groups, groups, process_set)
         for var in local_vars:
             _tape.register_local_source(var)
         return _tape
